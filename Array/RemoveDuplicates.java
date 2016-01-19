7af30b10b357d5e573a25c987555864e7623a179
Han <hanyu2@asu.edu>
2016-01-18 23:43:47 -0700

    26

---
+++ Array/RemoveDuplicates.java
@@ -0,0 +1,21 @@
+package Array;
+
+public class RemoveDuplicates {
+	 public static int removeDuplicates(int[] nums) {
+	        if (nums == null || nums.length == 0) {
+	            return 0;
+	        }
+	        
+	        int size = 0;
+	        for (int i = 0; i < nums.length; i++) {
+	            if (nums[i] != nums[size]) {
+	                nums[++size] = nums[i];
+	            }
+	        }
+	        return size + 1;
+	    }
+	 public static void main(String[] args) {
+		int nums [] = {1,1,2,3,3,4,5};
+		System.out.println(removeDuplicates(nums));
+	}
+}
