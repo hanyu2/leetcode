cd83ce26d7cff75ae8d008c7e3b07a6a58c012b6
Han <hanyu2@asu.edu>
2016-01-19 23:57:52 -0700

    BinarySearch

---
+++ BinarySearch/FirstBadVersion.java
@@ -0,0 +1,21 @@
+package BinarySearch;
+
+public class FirstBadVersion {
+	public int firstBadVersion(int n) {
+		 int start = 1, end = n;
+	        while (start + 1 < end) {
+	            int mid = start + (end - start) / 2;
+	            if (isBadVersion(mid)) {
+	                end = mid;
+	            } else {
+	                start = mid;
+	            }
+	        }
+	            
+	        if (isBadVersion(start)) {
+	            return start;
+	        }
+	        return end;
+	}
+    }
+}
