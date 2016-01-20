14a8e25cd4c4f1ab7b37f9df31b52bb274b70ff4
Han <hanyu2@asu.edu>
2016-01-20 00:17:06 -0700

    278

--- BinarySearch/FirstBadVersion.java
+++ BinarySearch/FirstBadVersion.java
@@ -1,21 +1,36 @@
 package BinarySearch;
 
 public class FirstBadVersion {
-	public int firstBadVersion(int n) {
-		 int start = 1, end = n;
-	        while (start + 1 < end) {
-	            int mid = start + (end - start) / 2;
-	            if (isBadVersion(mid)) {
-	                end = mid;
-	            } else {
-	                start = mid;
-	            }
-	        }
-	            
-	        if (isBadVersion(start)) {
-	            return start;
-	        }
-	        return end;
+	static int array[] = { 1, 1, 1, 1, 1, 1, 0, 0, 0 };
+
+	public static int firstBadVersion(int n) {
+
+		int start = 1, end = n;
+		while (start + 1 < end) {
+			int mid = start + (end - start) / 2;
+			if (isBadVersion(mid)) {
+				end = mid;
+			} else {
+				start = mid;
+			}
+		}
+
+		if (isBadVersion(start)) {
+			return start;
+		}
+		return end;
 	}
-    }
+	
+	public static boolean isBadVersion(int n){
+		if(array[n] == 1){
+			return false;
+		}else{
+			return true;
+		}
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(firstBadVersion(9));
+	}
+
 }
