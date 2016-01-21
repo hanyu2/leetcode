c297bdf94755a26af55f389e12a82dd0aeccf7bb
Han <hanyu2@asu.edu>
2016-01-20 21:05:01 -0700

    283

--- Array/MoveZeroes.java
+++ Array/MoveZeroes.java
@@ -2,26 +2,27 @@ package Array;
 
 public class MoveZeroes {
 	public static void moveZeroes(int[] nums) {
-		if (nums.length <= 1)
-			return;
-		int first = 0;
-		int sec = 0;
-		while (sec < nums.length) {
-			// find first zero element
-			while (first < nums.length && nums[first] != 0) {
-				first++;
-			}
-			if (first == nums.length)
-				return;
-			sec = first + 1;
-			// find following first non-zero element
-			while (sec < nums.length && nums[sec] == 0) {
-				sec++;
-			}
-			if (sec == nums.length)
-				return;
-			swap(nums, first, sec);
-		}
+		 if(nums.length <= 1){
+	            return;
+	        }
+	        int zero = 0;
+	        int nonzero = 0;
+	        while(nonzero < nums.length){
+	            while(zero < nums.length && nums[zero] != 0){
+	                zero++;
+	            }
+	            if(zero == nums.length){
+	                return;
+	            }
+	            nonzero = zero + 1;
+	            while(nonzero < nums.length && nums[zero] == 0){
+	                nonzero++;
+	            }
+	            if(nonzero == nums.length){
+	                return;
+	            }
+	            swap(nums, zero, nonzero);
+	        }
 	}
 
 	public static void swap(int[] nums, int i, int j) {
@@ -31,7 +32,7 @@ public class MoveZeroes {
 	}
 
 	public static void main(String[] args) {
-		int nums[] = { 0, 1, 0, 3, 12 };
+		int nums[] = {0, 1, 0, 3, 12};
 		moveZeroes(nums);
 	}
 }
