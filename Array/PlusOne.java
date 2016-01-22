20df0fa705a543836cdd139fe571173437f963bc
Han <hanyu2@asu.edu>
2016-01-22 14:37:28 -0700

    66

--- Array/PlusOne.java
+++ Array/PlusOne.java
@@ -2,35 +2,25 @@ package Array;
 
 public class PlusOne {
 	public static int[] plusOne(int[] digits) {
-		boolean flag = true;
-		for (int i = 0; i < digits.length; i++) {
-			flag &= (digits[i] == 9);
-			if(!flag){
-				break;
-			}
-		}
-		if(flag){
-			int newDigits [] = new int[digits.length + 1];
-			newDigits[0] = 1;
-			return newDigits;
-		}
-		else{
-			int i = digits.length - 1;
-			while(true){
-				if(digits[i] + 1 < 10){
-					digits[i] += 1;
-					break;
-				}else{
-					digits[i] = 0;
-					i--;
-				}
-			}
-		}
-		return digits;
+		int carries = 1;
+        for(int i = digits.length-1; i>=0 && carries > 0; i--){  // fast break when carries equals zero
+            int sum = digits[i] + carries;
+            digits[i] = sum % 10;
+            carries = sum / 10;
+        }
+        if(carries == 0)
+            return digits;
+            
+        int[] rst = new int[digits.length+1];
+        rst[0] = 1;
+        for(int i=1; i< rst.length; i++){
+            rst[i] = digits[i-1];
+        }
+        return rst;
 	}
 	
 	public static void main(String[] args) {
-		int num [] = {8,9,9,9};
+		int num [] = {9,9,9,9};
 		num = plusOne(num);
 		for (int i = 0; i < num.length; i++) {
 			System.out.print(num[i]);
