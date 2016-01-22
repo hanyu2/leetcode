b205cf508f454f8c10b2fcb9a457a6589edb9e8a
Han <hanyu2@asu.edu>
2016-01-22 16:51:09 -0700

    19

--- LinkedList/NthToTheEnd.java
+++ LinkedList/NthToTheEnd.java
@@ -2,22 +2,33 @@ package LinkedList;
 
 public class NthToTheEnd {
 	public static ListNode removeNthFromEnd(ListNode head, int n) {
-		ListNode dummy = new ListNode(0);  
-	    dummy.next = head;  
-	    ListNode node = dummy;  
-	    while(n-->0) head = head.next;  
-	    while(head != null) {  
-	        head = head.next;  
-	        node = node.next;  
-	    }  
-	    node.next = node.next.next;  
-	    return dummy.next;  
+		if (n <= 0) {
+            return null;
+        }
+        
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        
+        ListNode preDelete = dummy;
+        for (int i = 0; i < n; i++) {
+            if (head == null) {
+                return null;
+            }
+            head = head.next;
+        }
+        while (head != null) {
+            head = head.next;
+            preDelete = preDelete.next;
+        }
+        preDelete.next = preDelete.next.next;
+        return dummy.next;
 	}
-
 
 	public static void main(String[] args) {
 		ListNode n1 = new ListNode(1);
-		
+		ListNode n2 = new ListNode(2);
+		n1.next = n2;
+
 		n1 = removeNthFromEnd(n1, 1);
 		while (n1 != null) {
 			System.out.println(n1.val);
@@ -26,11 +37,8 @@ public class NthToTheEnd {
 	}
 }
 
-/*class ListNode {
-	int val;
-	ListNode next;
-
-	ListNode(int x) {
-		val = x;
-	}
-}*/
+/*
+ * class ListNode { int val; ListNode next;
+ * 
+ * ListNode(int x) { val = x; } }
+ */
