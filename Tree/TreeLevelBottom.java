64f24a676216b8151456716019f0e1c9042dadc4
Han <hanyu2@asu.edu>
2016-01-25 20:39:42 -0700

    107

---
+++ Tree/TreeLevelBottom.java
@@ -0,0 +1,28 @@
+package Tree;
+
+import java.util.ArrayList;
+
+public class TreeLevelBottom {
+	public static ArrayList<ArrayList<Integer>> levelOrderBottom(TreeNode root) {
+		
+	}
+
+	public static void main(String[] args) {
+		TreeNode n1 = new TreeNode(1);
+		TreeNode n2 = new TreeNode(2);
+		TreeNode n3 = new TreeNode(3);
+		TreeNode n4 = new TreeNode(4);
+		TreeNode n5 = new TreeNode(5);
+		TreeNode n6 = new TreeNode(6);
+		TreeNode n7 = new TreeNode(7);
+
+		n1.left = n2;
+		n1.right = n3;
+		n2.left = n4;
+		n2.right = n5;
+		n3.left = n6;
+		n3.right = n7;
+
+		ArrayList<ArrayList<Integer>> list = levelOrderBottom(n1);
+	}
+}
