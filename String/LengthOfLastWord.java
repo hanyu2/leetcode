d68842d9d123ac5fa646f62f3d6672fe9ed99e40
Han <hanyu2@asu.edu>
2016-01-27 12:55:44 -0700

    58

--- String/LengthOfLastWord.java
+++ String/LengthOfLastWord.java
@@ -2,7 +2,7 @@ package String;
 
 public class LengthOfLastWord {
 	public static int lengthOfLastWord(String s) {
-		if(s == null || s.length() == 0 || s == " "){
+		if(s == null || s.length() == 0 || s.trim().length() == 0){
             return 0;
 		}
         String [] strings = s.split(" ");
@@ -10,6 +10,27 @@ public class LengthOfLastWord {
 		
 	}
 
+	 public int lengthOfLastWord2(String s) {
+	       int length = 0;
+	        char[] chars = s.toCharArray();
+	        for (int i = s.length() - 1; i >= 0; i--) {
+	            if (length == 0) {
+	                if (chars[i] == ' ') {
+	                    continue;
+	                } else {
+	                    length++;
+	                }
+	            } else {
+	                if (chars[i] == ' ') {
+	                    break;
+	                } else {
+	                    length++;
+	                }
+	            }
+	        }
+
+	        return length;
+	    }
 	public static void main(String[] args) {
 		System.out.println(lengthOfLastWord("  "));
 	}
