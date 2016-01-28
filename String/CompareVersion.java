903457021b85f5be5f1f9dbcad715456b348884b
Han <hanyu2@asu.edu>
2016-01-27 23:28:26 -0700

    165

--- String/CompareVersion.java
+++ String/CompareVersion.java
@@ -2,45 +2,31 @@ package String;
 
 public class CompareVersion {
 	public static int compareVersion(String version1, String version2) {
-		String[] a = version1.split("\\.");
-		String[] b = version2.split("\\.");
-		if((a.length == 0 && b.length == 0) ||(a.length == 1 && b.length == 1)){
-			if(Integer.parseInt(version1) < Integer.parseInt(version2)){
-				return -1;
-			}else if(Integer.parseInt(version1) > Integer.parseInt(version2)){
-				return 1;
-			}else{
-				return 0;
-			}
-		}
-		int sa = Integer.parseInt(a[0]);
-		int sb = Integer.parseInt(b[0]);
-		if (sa < sb) {
-			return -1;
-		} else if (sa > sb) {
-			return 1;
-		} else {
-			int la = 0;
-			int lb = 0;
-			while (la == a[1].length() - 1 && lb == b[1].length() - 1) {
-				if (((int) a[1].charAt(la) - '0') == ((int) b[1].charAt(lb) - '0')) {
-					la++;
-					lb++;
-					continue;
-				} else if (((int) a[1].charAt(la) - '0') > ((int) b[1].charAt(lb) - '0')) {
-					return 1;
-				} else {
-					return -1;
-				}
-			}
-			if (la < a[1].length() - 1) {
-				return 1;
-			} else if(lb < b[1].length() - 1){
-				return -1;
-			}else{
-				return 0;
-			}
-		}
+		String[] str1=version1.split("\\.");  
+	    String[] str2=version2.split("\\.");  
+	    //获取需要比例的长度为两版本中长度的大者  
+	    int length=Math.max(str1.length, str2.length);  
+	    for (int i = 0; i < length; i++)  
+	    {  
+	        /* 
+	         * 下面两行代码的意思将两版本的长度设置成一样 
+	         * 如version1=1.1 version2=1.1.2 
+	         * 执行完 int num1=i<str1.length?Integer.parseInt(str1[i]):0 后 
+	         * version1=1.1.0，这样方便比较 
+	         */  
+	        int num1=i<str1.length?Integer.parseInt(str1[i]):0;  
+	        int num2=i<str2.length?Integer.parseInt(str2[i]):0;  
+	          
+	        if (num1>num2)  
+	        {  
+	            return 1;  
+	        }  
+	        else if (num1<num2)  
+	        {  
+	            return -1;  
+	        }  
+	    }  
+	    return 0;    
 	}
 	
 	public static void main(String[] args) {
