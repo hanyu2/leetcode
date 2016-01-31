30918f78ff748a252b7ea2e2519ebfbf9477886f
Han <hanyu2@asu.edu>
2016-01-31 15:21:26 -0700

    205

---
+++ Hashtable/IsomorphicString.java
@@ -0,0 +1,28 @@
+package Hashtable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class IsomorphicString {
+	public static boolean isIsomorphic(String s, String t) {
+		if(s.length() != t.length()) return false;  
+        if(s.length() <=1) return true;  
+        Map<Character, Character> dic = new HashMap<Character, Character>();  
+        for(int i=0; i<s.length(); i++) {  
+            char s1 = s.charAt(i);  
+            char t1 = t.charAt(i);  
+            if(!dic.containsKey(s1)) {  
+                if(dic.containsValue(t1)) return false;  
+                else dic.put(s1, t1);  
+            }  
+            else {  
+                if(!dic.get(s1).equals(t1)) return false;  
+            }  
+        }  
+        return true;  
+	}
+	public static void main(String[] args) {
+		System.out.println(isIsomorphic("abcdefghi", ""));
+		
+	}
+}
