2f1111889cc5f7155cbfa0db25d4e47c80c09a2a
Han <hanyu2@asu.edu>
2016-02-01 22:56:14 -0700

    278

--- BinarySearch/FirstBadVersion.java
+++ BinarySearch/FirstBadVersion.java
@@ -5,20 +5,17 @@ public class FirstBadVersion {
 
 	public static int firstBadVersion(int n) {
 
-		int start = 1, end = n;
-		while (start + 1 < end) {
-			int mid = start + (end - start) / 2;
-			if (isBadVersion(mid)) {
-				end = mid;
-			} else {
-				start = mid;
-			}
-		}
-
-		if (isBadVersion(start)) {
-			return start;
-		}
-		return end;
+		int low=1, high=n;  
+        
+        while(low <= high) {  
+            int mid=low + (high-low)/2;  
+            if(isBadVersion(mid)) {  
+                high = mid - 1;  
+            } else {  
+                low = mid + 1;  
+            }  
+        }  
+        return low; 
 	}
 	
 	public static boolean isBadVersion(int n){
