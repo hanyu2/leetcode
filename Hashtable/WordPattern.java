41c71ce6753c16c015a668acee3be9891ba8add8
Han <hanyu2@asu.edu>
2016-02-01 21:04:31 -0700

    290

---
+++ Hashtable/WordPattern.java
@@ -0,0 +1,54 @@
+package Hashtable;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class WordPattern {
+	public static boolean wordPattern(String pattern, String str) {
+		String[] strings = str.split(" ");
+		if (pattern.length() != strings.length)
+			return false;
+		Map<String, Character> map = new HashMap<String, Character>();
+		for (int i = 0; i < strings.length; i++) {
+			if (!map.containsKey(strings[i])) {
+				if (map.containsValue(pattern.charAt(i))) {
+					return false;
+				}
+				map.put(strings[i], pattern.charAt(i));
+			} else {
+				if (map.get(strings[i]).equals(pattern.charAt(i))) {
+					continue;
+				} else {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	 public static boolean wordPattern2(String pattern, String str) {
+         
+	        if (pattern.isEmpty() || str.isEmpty()) {
+	            return false;
+	        }
+	         
+	        String[] s = str.split(" ");
+	        if (s.length != pattern.length()) {
+	            return false;
+	        }
+	         
+	        @SuppressWarnings("rawtypes")
+	        HashMap<Comparable, Integer> hashMap = new HashMap<Comparable, Integer>();
+	        for (int i = 0; i < pattern.length(); i++) {
+	            if (!Objects.equals(hashMap.put(pattern.charAt(i), i), hashMap.put(s[i], i)))
+	                return false;
+	        }
+	         
+	        return true;
+	    }
+
+	public static void main(String[] args) {
+		System.out.println(wordPattern2("abba", "dog cat cat dog"));
+	}
+}
