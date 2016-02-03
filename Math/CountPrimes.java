005c89ee87ab7abfed7eb45c93895cef429202d1
Han <hanyu2@asu.edu>
2016-02-03 12:19:54 -0700

    204

--- Math/CountPrimes.java
+++ Math/CountPrimes.java
@@ -18,8 +18,26 @@ public class CountPrimes {
 		}
 		return c;
 	}
-
+	public static int countPrimes2(int n) {
+		   boolean[] isPrime = new boolean[n];
+		   for (int i = 2; i < n; i++) {
+		      isPrime[i] = true;
+		   }
+		   // Loop's ending condition is i * i < n instead of i < sqrt(n)
+		   // to avoid repeatedly calling an expensive function sqrt().
+		   for (int i = 2; i * i < n; i++) {
+		      if (!isPrime[i]) continue;
+		      for (int j = i * i; j < n; j += i) {
+		         isPrime[j] = false;
+		      }
+		   }
+		   int count = 0;
+		   for (int i = 2; i < n; i++) {
+		      if (isPrime[i]) count++;
+		   }
+		   return count;
+		}
 	public static void main(String[] args) {
-		System.out.println(countPrimes(30));
+		System.out.println(countPrimes2(30));
 	}
 }
