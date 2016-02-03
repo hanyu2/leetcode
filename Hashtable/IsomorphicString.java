fe2dd17ee2ed287c9dad04ffff21d98b67517dbb
Han <hanyu2@asu.edu>
2016-02-03 12:19:32 -0700

    205

--- Hashtable/IsomorphicString.java
+++ Hashtable/IsomorphicString.java
@@ -5,40 +5,37 @@ import java.util.Map;
 
 public class IsomorphicString {
 	public static boolean isIsomorphic(String s, String t) {
-		if(s.length() != t.length()) return false;  
-        Map<Character, Character> dic = new HashMap<Character, Character>();  
-        for(int i=0; i<s.length(); i++) {  
-            char s1 = s.charAt(i);  
-            char t1 = t.charAt(i);  
-            if(!dic.containsKey(s1)) {  
-                if(dic.containsValue(t1)) return false;  
-                else dic.put(s1, t1);  
-            }  
-            else {  
-                if(!dic.get(s1).equals(t1)) return false;  
-            }  
-        }  
-        return true;  
+		if (s.length() != t.length())
+			return false;
+		Map<Character, Character> dic = new HashMap<Character, Character>();
+		for (int i = 0; i < s.length(); i++) {
+			char s1 = s.charAt(i);
+			char t1 = t.charAt(i);
+			if (!dic.containsKey(s1)) {
+				if (dic.containsValue(t1))
+					return false;
+				else
+					dic.put(s1, t1);
+			} else {
+				if (!dic.get(s1).equals(t1))
+					return false;
+			}
+		}
+		return true;
 	}
-	
+
 	public static boolean isIsomorphic2(String s, String t) {
-        if(s.length() != t.length()){
-            return false;
-        }
-        Map<Character, Character> map = new HashMap<Character, Character>();
-        for(int i = 0; i < s.length(); i++){
-            if(!map.containsKey(s.charAt(i))){
-                map.put(s.charAt(i), t.charAt(i));
-            }else{
-                if(map.get(s.charAt(i)) != t.charAt(i)){
-                	return false;
-                }
-            }
-        }
-        return true;
-    }
+		int[] m = new int[512];
+		for (int i = 0; i < s.length(); i++) {
+			if (m[s.charAt(i)] != m[t.charAt(i) + 256])
+				return false;
+			m[s.charAt(i)] = m[t.charAt(i) + 256] = i + 1;
+		}
+		return true;
+	}
+
 	public static void main(String[] args) {
 		System.out.println(isIsomorphic2("ab", "aa"));
-		
+
 	}
 }
