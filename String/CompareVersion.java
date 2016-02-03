6c7d18368f7a1e0ab1dd4468c49c539949c513af
Han <hanyu2@asu.edu>
2016-02-03 14:04:00 -0700

    165

--- String/CompareVersion.java
+++ String/CompareVersion.java
@@ -27,9 +27,33 @@ public class CompareVersion {
 	        }  
 	    }  
 	    return 0;    
-	}
+	}	
 	
+	 public static int compareVersion2(String version1, String version2) {
+	      String[] verArr1 = version1.split("\\.");
+	        String[] verArr2 = version2.split("\\.");
+	        int index1 = 0;
+	        int index2 = 0;
+	        while (index1 < verArr1.length || index2 < verArr2.length) {
+	            int val1 = 0;
+	            int val2 = 0;
+	            if (index1 < verArr1.length) {
+	                val1 = Integer.parseInt(verArr1[index1++]);//Do not foget increment here!!!
+	            }
+	            if (index2 < verArr2.length) {
+	                val2 = Integer.parseInt(verArr2[index2++]);
+	            }
+	            if (val1 < val2) {
+	                return -1;
+	            }
+	            if (val1 > val2) {
+	                return 1;
+	            }
+	        }
+
+	        return 0;
+	    }
 	public static void main(String[] args) {
-		System.out.println(compareVersion("01", "1"));
+		System.out.println(compareVersion2("01", "1"));
 	}
 }
