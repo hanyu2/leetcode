f017d14e33c8712794be88e7e0f85b0d102c4781
Han <hanyu2@asu.edu>
2016-02-04 01:20:09 -0700

    112

--- Tree/PathSum.java
+++ Tree/PathSum.java
@@ -1,23 +1,28 @@
 package Tree;
 
 public class PathSum {
-	public static boolean hasPathSum(TreeNode root, int sum) {
-		if (root == null) {
+	public static boolean hasPathSum(TreeNode root, int sum){
+		if(root == null){
 			return false;
 		}
-		int result = 0;
-		return check(root, sum, result);
+		if(root.left == null && root.right == null){
+			return sum - root.val == 0;
+		}
+		return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+	}
+	
+	public static boolean hasPathSum2(TreeNode root, int sum) {
+		return check(root, 0, sum);
 	}
 
-	public static boolean check(TreeNode root, int num, int sum) {
+	public static boolean check(TreeNode root, int result, int sum) {
 		if (root == null) {
 			return false;
 		}
-		int newSum = sum + root.val;
-		if (newSum == num) {
-			return true;
+		if(root.left == null && root.right == null){
+			return result + root.val == sum;
 		}
-		return check(root.left, num, newSum) && check(root.right, num, newSum);
+		return check(root.left, result + root.val, sum) || check(root.right, result + root.val, sum);
 	}
 
 	public static void main(String[] args) {
