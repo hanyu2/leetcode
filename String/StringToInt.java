ac1ce27ef41aeca05bb57503494b94e9cb1b10be
Han <hanyu2@asu.edu>
2016-02-06 14:26:21 -0700

    8

--- String/StringToInt.java
+++ String/StringToInt.java
@@ -19,7 +19,7 @@ public class StringToInt {
 			sign = -1;
 			index++;
 		}
-		long num = 0;
+		long num = 0;/////////////////////notice!
 		for (; index < str.length(); index++) {
 			if (str.charAt(index) < '0' || str.charAt(index) > '9')
 				break;
@@ -34,38 +34,7 @@ public class StringToInt {
 		if (num * sign <= Integer.MIN_VALUE) {
 			return Integer.MIN_VALUE;
 		}
-		return (int) num * sign;
-	}
-
-	public static int myAtoi2(String str) {
-		if (str == null || str.length() == 0) {
-			return 0;
-		}
-		str = str.trim();
-		int index = 0;
-		int sign = 1;
-		int num = 0;
-		if (str.charAt(index) == '+') {
-			index++;
-		} else if (str.charAt(index) == '-') {
-			sign = -1;
-			index++;
-		}
-		for (; index < str.length(); index++) {
-			if (str.charAt(index) < '0' || str.charAt(index) > '9') {
-				break;
-			}
-			num = num * 10 + (str.charAt(index) - '0');
-			if (num > Integer.MAX_VALUE) {
-				break;
-			}
-		}
-		if (sign * num >= Integer.MAX_VALUE) {
-			return Integer.MAX_VALUE;
-		} else if (sign * num <= Integer.MIN_VALUE) {
-			return Integer.MIN_VALUE;
-		}
-		return sign * num;
+		return (int) num * sign; ///////////notice cannot (int) sign *  num
 	}
 
 	public static void main(String[] args) {
