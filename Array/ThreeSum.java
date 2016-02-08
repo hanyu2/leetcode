0b23145e1d3f3daa51d1e2034db6e32ce4d342f8
Han <hanyu2@asu.edu>
2016-02-08 09:47:57 -0700

    15

--- Array/ThreeSum.java
+++ Array/ThreeSum.java
@@ -2,12 +2,13 @@ package Array;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 public class ThreeSum {
 	public static List<List<Integer>> threeSum(int[] nums) {
 		List<List<Integer>> result = new ArrayList<List<Integer>>();
-		if(nums == null || nums.length < 3){
+		if (nums == null || nums.length < 3) {
 			return result;
 		}
 		Arrays.sort(nums);
@@ -21,17 +22,11 @@ public class ThreeSum {
 			while (left < right) {
 				int sum = nums[left] + nums[right] + nums[i];
 				if (sum == 0) {
-					ArrayList<Integer> tmp = new ArrayList<Integer>();
-					tmp.add(nums[i]);
-					tmp.add(nums[left]);
-					tmp.add(nums[right]);
-					result.add(tmp);
-					left++;
-					right--;
-					while (left < right && nums[left] == nums[left - 1]) { // to skip duplicates
+					result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+					while (left < right && nums[left] == nums[left + 1]) {
 						left++;
 					}
-					while (left < right && nums[right] == nums[right + 1]) { // to skip duplicates
+					while (left < right && nums[right] == nums[right - 1]) {
 						right--;
 					}
 				} else if (sum < 0) {
@@ -43,8 +38,34 @@ public class ThreeSum {
 		}
 		return result;
 	}
+
+	public static List<List<Integer>> threeSum2(int[] nums) {
+		Arrays.sort(nums);
+		List<List<Integer>> res = new LinkedList<>();
+		for (int i = 0; i < nums.length - 2; i++) {
+			if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
+				int lo = i + 1, hi = nums.length - 1, sum = 0 - nums[i];
+				while (lo < hi) {
+					if (nums[lo] + nums[hi] == sum) {
+						res.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
+						while (lo < hi && nums[lo] == nums[lo + 1])
+							lo++;
+						while (lo < hi && nums[hi] == nums[hi - 1])
+							hi--;
+						lo++;
+						hi--;
+					} else if (nums[lo] + nums[hi] < sum)
+						lo++;
+					else
+						hi--;
+				}
+			}
+		}
+		return res;
+	}
+
 	public static void main(String[] args) {
-		int nums [] = {0, 0, 0, 0};
-		threeSum(nums);
+		int nums[] = {-1, 0, 1};
+		threeSum2(nums);
 	}
 }
