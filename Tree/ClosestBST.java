f81ca15e95ce776084286d3de5cc7c0356c29e83
Han <hanyu2@asu.edu>
2016-02-09 21:28:00 -0700

    270

---
+++ Tree/ClosestBST.java
@@ -0,0 +1,26 @@
+package Tree;
+
+public class ClosestBST {
+	public int closestValue(TreeNode root, double target) {
+
+		int closest = root.val;
+		double min = Double.MAX_VALUE;
+
+		while (root != null) {
+			if (Math.abs(root.val - target) < min) {
+				min = Math.abs(root.val - target);
+				closest = root.val;
+			}
+
+			if (target < root.val) {
+				root = root.left;
+			} else if (target > root.val) {
+				root = root.right;
+			} else {
+				return root.val;
+			}
+		}
+
+		return closest;
+	}
+}
