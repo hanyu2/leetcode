14df861e668a7c2d1683b4046a0d9586ed4c5a08
Han <hanyu2@asu.edu>
2016-02-19 14:14:59 -0700

    75

---
+++ Array/SortColors.java
@@ -0,0 +1,62 @@
+package Array;
+
+public class SortColors {
+	public static void sortColors(int[] nums) {
+       int n = nums.length - 1;
+       int count  = 0;
+       for(int i = 0; i < n; i++){
+    	   while(nums[i] == 2 && i < n) swap(nums, i, n--);
+    	   while(nums[i] == 0 && i > count) swap(nums, i, count++);
+       }
+	}
+	public static void swap(int nums[], int i, int j){
+		nums[i] ^= nums[j];
+		nums[j] ^= nums[i];
+		nums[i] ^= nums[j];
+	}
+	
+	public static void sortColors2(int [] nums){
+		int n0 = -1, n1 = -1, n2 = -1;
+		for(int i = 0; i < nums.length; i++){
+			if(nums[i] == 0){
+				nums[++n2] = 2;
+				nums[++n1] = 1;
+				nums[++n0] = 0;
+			}else if(nums[i] == 1){
+				nums[++n2] = 2;
+				nums[++n1] = 1;
+			}else{
+				nums[++n2] = 2;
+			}
+		}
+	}
+	
+	public static void sortColors3(int [] nums){
+		int j = 0, k = nums.length - 1;
+		for(int i = 0; i <= k; i++){
+			if(nums[i] == 0 && i != j){
+				swap(nums, i--, j++);
+			}else{
+				if(nums[i] == 2 && i != k){
+					swap(nums, i--, k--);
+				}
+			}
+		}
+	}
+	
+	public static void sortColors4(int [] nums){
+		int j = 0, k = nums.length - 1;
+		for(int i = 0; i <= k; i++){
+			if(nums[i] == 0){
+				swap(nums, i, j++);
+			}else if(nums[i] == 2){
+				swap(nums, i--, k--);
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		int nums [] = {1,2,0,1,0,2,0,1};
+		sortColors4(nums);
+	}
+}
