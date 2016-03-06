758129755bafc4d8dd5318999df06a691421f57d
Han <hanyu2@asu.edu>
2016-03-06 15:12:16 -0700

    162

---
+++ Array/PeakElement.java
@@ -0,0 +1,51 @@
+package Array;
+
+public class PeakElement {
+	// Binary Search recursion
+	public static int findPeakElement(int[] nums) {
+		return findHelper(nums, 0, nums.length - 1);
+	}
+
+	public static int findHelper(int[] nums, int low, int high) {
+		if (low == high) {
+			return low;
+		} else {
+			int mid = (low + high) / 2;
+			if (nums[mid] > nums[mid + 1]) {
+				return findHelper(nums, low, mid);
+			} else {
+				return findHelper(nums, mid + 1, high);
+			}
+		}
+	}
+
+	// Binary Search: iteration
+	public static int findPeakElement2(int[] nums) {
+		int low = 0;
+		int high = nums.length - 1;
+		while (low < high) {
+			int mid = (low + high) / 2;
+			if (nums[mid] < nums[mid + 1]) {
+				low = mid + 1;
+			} else {
+				high = mid;
+			}
+		}
+		return low;
+	}
+
+	// Interation Sequential search O(N)
+	public int findPeakElement3(int[] nums) {
+		for (int i = 1; i < nums.length; i++) {
+			if (nums[i] < nums[i - 1]) {
+				return i - 1;
+			}
+		}
+		return nums.length - 1;
+	}
+	
+	public static void main(String[] args) {
+		int nums[] = { 2, 3, 4, 3, 2, 1 };
+		System.out.println(findPeakElement(nums));
+	}
+}
