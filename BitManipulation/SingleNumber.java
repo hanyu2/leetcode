7d9973defad39dc6bf903fcd9eccd79e6f7d4449
Han <hanyu2@asu.edu>
2016-03-18 14:48:39 -0700

    136

---
+++ BitManipulation/SingleNumber.java
@@ -0,0 +1,28 @@
+package BitManipulation;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SingleNumber {
+	public int singleNumber(int[] nums) {
+		Set<Integer> set = new HashSet<Integer>();
+		int result = 0;
+		for(Integer i : nums){
+			if(set.contains(i)){
+				result -= i;
+				set.remove(i);
+			}else{
+				result += i;
+				set.add(i);
+			}
+		}
+		return result;
+	}
+	public int singleNumber2(int[] nums){
+		int result = 0;
+		for(Integer i : nums){
+			result ^= i;
+		}
+		return result;
+	}
+}
