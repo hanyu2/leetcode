eda7f3558b220278015e660dead6adfb9a752303
Han <hanyu2@asu.edu>
2016-03-20 18:23:02 -0700

    275

--- BinarySearch/HIndex2.java
+++ BinarySearch/HIndex2.java
@@ -2,21 +2,26 @@ package BinarySearch;
 
 public class HIndex2 {
 	public static int hIndex(int[] citations) {
-        int start = 0;
-        int end = citations.length - 1;
-        int n = citations.length;
-        while(start <= end){
-            int middle = (start + end) / 2;
-            if(citations[middle] >= n - middle){
-                end  = middle - 1;
-            }else{
-            	start = start + 1;
-            }
-        }
-        return n - start;
-    }
+		int start = 0;
+		int end = citations.length - 1;
+		int n = citations.length;
+		while (start <= end) {
+			int middle = (start + end) / 2;
+			if (citations[middle] == n - middle) {
+				return n - middle;
+			}
+			if (citations[middle] > n - middle) {
+				end = middle - 1;
+			} else {
+				start = middle + 1;
+			}
+		}
+		return n - start;
+	}
+
 	public static void main(String[] args) {
-		int citations[] = {0};
+		// int citations[] = {0};
+		int citations[] = { 1, 2, 3, 8, 9 };
 		System.out.println(hIndex(citations));
 	}
 }
