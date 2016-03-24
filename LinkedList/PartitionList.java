09e6695225147051d09f2eb1b26ebd246a2719dd
Han <hanyu2@asu.edu>
2016-03-24 14:05:19 -0700

    86

---
+++ LinkedList/PartitionList.java
@@ -0,0 +1,27 @@
+package LinkedList;
+
+public class PartitionList {
+	public static ListNode partition(ListNode head, int x) {
+		ListNode dummy1 = new ListNode(0), dummy2 = new ListNode(0);  //dummy heads of the 1st and 2nd queues
+	    ListNode curr1 = dummy1, curr2 = dummy2;      //current tails of the two queues;
+	    while (head!=null){
+	        if (head.val<x) {
+	            curr1.next = head;
+	            curr1 = head;
+	        }else {
+	            curr2.next = head;
+	            curr2 = head;
+	        }
+	        head = head.next;
+	    }
+	    curr2.next = null;          //important! avoid cycle in linked list. otherwise u will get TLE.
+	    curr1.next = dummy2.next;
+	    return dummy1.next;
+    }
+	public static void main(String[] args) {
+		ListNode n1 = new ListNode(1);
+		ListNode n2 = new ListNode(1);
+		n1.next = n2;
+		partition(n1, 2);
+	}
+}
