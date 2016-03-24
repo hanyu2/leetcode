725ff42b7ba9031eabf1bb0042ad4e7e93038f81
Han <hanyu2@asu.edu>
2016-03-24 14:05:32 -0700

    147

---
+++ LinkedList/InsertionSortList.java
@@ -0,0 +1,39 @@
+package LinkedList;
+
+public class InsertionSortList {
+	public static ListNode insertionSortList(ListNode head) {
+		if( head == null ){
+            return head;
+        }
+
+        ListNode dummy = new ListNode(0); //new starter of the sorted list
+        ListNode cur = head; //the node will be inserted
+        ListNode pre = dummy; //insert node between pre and pre.next
+        ListNode next = null; //the next node will be inserted
+        //not the end of input list
+        while( cur != null ){
+            next = cur.next;
+            //find the right place to insert
+            while( pre.next != null && pre.next.val < cur.val ){
+                pre = pre.next;
+            }
+            //insert between pre and pre.next
+            cur.next = pre.next;
+            pre.next = cur;
+            pre = dummy;
+            cur = next;
+        }
+
+        return dummy.next;
+    }
+    
+    
+    public static void main(String[] args) {
+    	ListNode n1 = new ListNode(3);
+		ListNode n2 = new ListNode(4);
+		ListNode n3 = new ListNode(1);
+		n1.next = n2;
+		n2.next = n3;
+		insertionSortList(n1);
+	}
+}
