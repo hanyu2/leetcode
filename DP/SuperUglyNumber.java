638ed82cc2c2047db5940d37c7e68bf6dc248a4d
Han <hanyu2@asu.edu>
2016-03-26 13:40:41 -0700

    313

--- DP/SuperUglyNumber.java
+++ DP/SuperUglyNumber.java
@@ -28,45 +28,43 @@ public class SuperUglyNumber {
     }
 	
 	//Min heap O( log(k)N )
-	public int nthSuperUglyNumberHeap2(int n, int[] primes) {
-	    int[] ugly = new int[n];
-
-	    PriorityQueue<Num> pq = new PriorityQueue<>();
-	    for (int i = 0; i < primes.length; i++) pq.add(new Num(primes[i], 1, primes[i]));
-	    ugly[0] = 1;
-
-	    for (int i = 1; i < n; i++) {
-	        ugly[i] = pq.peek().val;
-	        while (pq.peek().val == ugly[i]) {
-	            Num nxt = pq.poll();
-	            pq.add(new Num(nxt.p * ugly[nxt.idx], nxt.idx + 1, nxt.p));
-	        }
-	    }
-
-	    return ugly[n - 1];
-	}
-
-	private class Num implements Comparable<Num> {
-	    int val;
-	    int idx;
-	    int p;
-
-	    public Num(int val, int idx, int p) {
-	        this.val = val;
-	        this.idx = idx;
-	        this.p = p;
-	    }
-
-	    @Override
-	    public int compareTo(Num that) {
-	        return this.val - that.val;
-	    }
-	}
+	public int nthSuperUglyNumber2(int n, int[] primes) {
+        int[] ugly = new int[n];
+        ugly[0] = 1;
+        PriorityQueue<Num> q = new PriorityQueue<Num>();
+        for(int i = 0; i < primes.length; i++){
+            q.add(new Num(primes[i], 1, primes[i]));
+        }
+        for(int i = 1; i < n; i++){
+            ugly[i] = q.peek().nextValue;
+            while(q.peek().nextValue == ugly[i]){
+                Num num = q.poll();
+                q.add(new Num(ugly[num.index] * num.value, num.index + 1, num.value));
+            }
+        }
+        return ugly[n - 1];
+    }
+    private class Num implements Comparable<Num>{
+        int nextValue;
+        int index;
+        int value;
+        
+        public Num(int nextValue, int index, int value){
+            this.nextValue = nextValue;
+            this.index = index;
+            this.value = value;
+        }
+        
+        @Override
+        public int compareTo(Num that){
+            return this.nextValue - that.nextValue;
+        }
+    }
 	
 	public static void main(String[] args) {
 		int [] primes = {2, 7, 13, 19};
 		//System.out.println(nthSuperUglyNumber(12, primes));
 		SuperUglyNumber sun = new SuperUglyNumber();
-		System.out.println(sun.nthSuperUglyNumberHeap2(12, primes));
+		System.out.println(sun.nthSuperUglyNumber2(12, primes));
 	}
 }
