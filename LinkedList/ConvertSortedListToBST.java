982bc863a8739c45212529de23de84ad8d0d7e9f
Han <hanyu2@asu.edu>
2016-03-26 11:37:59 -0700

    109

--- LinkedList/ConvertSortedListToBST.java
+++ LinkedList/ConvertSortedListToBST.java
@@ -62,13 +62,35 @@ public class ConvertSortedListToBST {
 	    root.right = sortedListToBST(slow.next);
 	    return root;
 	}
+	
+	public TreeNode sortedListToBST3(ListNode head) {
+        if(head==null){
+            return null;
+        }
+        if(head.next==null){
+            TreeNode treeNode=new TreeNode(head.val);
+            return treeNode;
+        }
+        ListNode slow=head;
+        ListNode fast=head.next.next;
+        while(fast!=null&&fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        TreeNode root=new TreeNode(slow.next.val);
+        ListNode temp=slow.next.next;
+        slow.next=null;
+        root.left=sortedListToBST(head);
+        root.right=sortedListToBST(temp);
+        return root;
+    }
 
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		ListNode n1 = new ListNode(1);
 		ListNode n2 = new ListNode(3);
 
 		n1.next = n2;
 		sortedListToBST(n1);
 
-	}
+	}*/
 }
