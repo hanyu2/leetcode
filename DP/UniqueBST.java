94d9b58b3fba007ba291d2979eb9a9e3fb99fa55
Han <hanyu2@asu.edu>
2016-04-06 12:25:17 -0700

    96

--- DP/UniqueBST.java
+++ DP/UniqueBST.java
@@ -2,10 +2,9 @@ package DP;
 
 public class UniqueBST {
 	//https://leetcode.com/discuss/24282/dp-solution-in-6-lines-with-explanation-f-i-n-g-i-1-g-n-i
-	public int numTrees(int n) {
+	public static int numTrees(int n) {
 	    int [] G = new int[n+1];
 	    G[0] = G[1] = 1;
-
 	    for(int i=2; i<=n; ++i) {
 	        for(int j=1; j<=i; ++j) {
 	            G[i] += G[j-1] * G[i-j];
@@ -14,5 +13,7 @@ public class UniqueBST {
 
 	    return G[n];
 	}
-	
+	public static void main(String[] args) {
+		System.out.println(numTrees(5));
+	}
 }
