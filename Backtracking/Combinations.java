2fc72eacc0612a4158c30718078ac944e03f0ce6
Han <hanyu2@asu.edu>
2016-04-11 23:08:51 -0700

    77

---
+++ Backtracking/Combinations.java
@@ -0,0 +1,48 @@
+package Backtracking;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Combinations {
+	public static List<List<Integer>> combine(int n, int k) {
+		List<List<Integer>> res = new ArrayList<List<Integer>>();
+		helper(res, new ArrayList<Integer>(), n, 1, k);
+		return res;
+	}
+
+	public static void helper(List<List<Integer>> res, List<Integer> list, int n, int index, int k) {
+		if (k == 0) {
+			res.add(list);
+			return;
+		}
+		for (int i = index; i <= n; i++) {
+			List<Integer> temp = new ArrayList<Integer>(list);
+			temp.add(i);
+			helper(res, temp, n, i + 1, k - 1);
+		}
+	}
+	//Iterative
+	public static List<List<Integer>> combine2(int n, int k) {
+	    if (n == 0 || k == 0 || k > n) return Collections.emptyList();
+	    List<List<Integer>> res = new ArrayList<List<Integer>>();
+	    for (int i = 1; i <= n + 1 - k; i++) res.add(Arrays.asList(i));
+	    for (int i = 2; i <= k; i++) {
+	        List<List<Integer>> tmp = new ArrayList<List<Integer>>();
+	        for (List<Integer> list : res) {
+	            for (int m = list.get(list.size() - 1) + 1; m <= n - (k - i); m++) {
+	                List<Integer> newList = new ArrayList<Integer>(list);
+	                newList.add(m);
+	                tmp.add(newList);
+	            }
+	        }
+	        res = tmp;
+	    }
+	    return res;
+	}
+
+	public static void main(String[] args) {
+		combine2(4, 3);
+	}
+}
