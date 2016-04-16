32aa6ca202730573aed9df908e93c239c664b651
Han <hanyu2@asu.edu>
2016-04-15 20:15:43 -0700

    201

---
+++ BitManipulation/BitwiseANDOfNumbersRange.java
@@ -0,0 +1,21 @@
+package BitManipulation;
+
+public class BitwiseANDOfNumbersRange {
+	// https://yq.aliyun.com/articles/3512
+	public int rangeBitwiseAnd(int m, int n) {
+		int count = 0;
+		while (m != n) {
+			m = m >>> 1; // shift numbers right
+			n = n >>> 1; // shift numbers right
+			count = count + 1; // keep track of Shifts
+		}
+		return n << count; // do left shifts with 0 padding and return answer.
+	}
+
+	public int rangeBitwiseAnd2(int m, int n) {
+		int r = Integer.MAX_VALUE;
+		while ((m & r) != (n & r))
+			r = r << 1;
+		return n & r;
+	}
+}
