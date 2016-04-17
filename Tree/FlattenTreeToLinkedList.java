79b1419d3f4178628e15ca834da4f5a2acdac6ff
Han <hanyu2@asu.edu>
2016-04-17 11:35:48 -0700

    114

---
+++ Tree/FlattenTreeToLinkedList.java
@@ -0,0 +1,51 @@
+package Tree;
+
+import java.util.Stack;
+
+public class FlattenTreeToLinkedList {
+	private TreeNode prev = null;
+
+	public void flatten(TreeNode root) {
+		if (root == null)
+			return;
+		flatten(root.right);
+		flatten(root.left);
+		root.right = prev;
+		root.left = null;
+		prev = root;
+	}
+	//straight forward
+	public void flatten2(TreeNode root) {
+        if (root == null) return;
+
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+
+        root.left = null;
+
+        flatten(left);
+        flatten(right);
+
+        root.right = left;
+        TreeNode cur = root;
+        while (cur.right != null) cur = cur.right;
+        cur.right = right;
+    }
+	
+	//DFS Use a stack
+	public void flatten3(TreeNode root) {
+        if (root == null) return;
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+        while (!stack.isEmpty()){
+            TreeNode curr = stack.pop();
+            if (curr.right!=null)  
+                 stack.push(curr.right);
+            if (curr.left!=null)  
+                 stack.push(curr.left);
+            if (!stack.isEmpty()) 
+                 curr.right = stack.peek();
+            curr.left = null;  // dont forget this!! 
+        }
+    }
+}
