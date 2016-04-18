3145ee886253a245094ff77541a8e82385ef5f33
Han <hanyu2@asu.edu>
2016-04-18 11:10:37 -0700

    199

---
+++ Tree/BinaryTreeRightSideView.java
@@ -0,0 +1,64 @@
+package Tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BinaryTreeRightSideView {
+	// BFS
+	public List<Integer> rightSideView(TreeNode root) {
+		List<Integer> res = new ArrayList<Integer>();
+		Queue<TreeNode> q = new LinkedList<TreeNode>();
+		if (root == null) {
+			return res;
+		}
+		q.add(root);
+		while (!q.isEmpty()) {
+			int size = q.size();
+			for (int i = 0; i < size; i++) {
+				TreeNode temp = q.poll();
+				if (i == size - 1) {
+					res.add(temp.val);
+				}
+				if (temp.left != null) {
+					q.offer(temp.left);
+				}
+				if (temp.right != null) {
+					q.offer(temp.right);
+				}
+			}
+		}
+		return res;
+	}
+	//Better
+	public static List<Integer> rightSideView2(TreeNode root) {
+		List<Integer> result = new ArrayList<Integer>();
+		rightView(root, result, 0);
+		return result;
+	}
+
+	public static void rightView(TreeNode curr, List<Integer> result, int currDepth) {
+		if (curr == null) {
+			return;
+		}
+		if (currDepth == result.size()) {
+			result.add(curr.val);
+		}
+
+		rightView(curr.right, result, currDepth + 1);
+		rightView(curr.left, result, currDepth + 1);
+
+	}
+	
+	public static void main(String[] args) {
+		TreeNode n1 = new TreeNode(1);
+		TreeNode n2 = new TreeNode(2);
+		TreeNode n3 = new TreeNode(3);
+		TreeNode n4 = new TreeNode(4);
+		n1.left = n2;
+		n1.right = n3;
+		n2.right = n4;
+		rightSideView2(n1);
+	}
+}
