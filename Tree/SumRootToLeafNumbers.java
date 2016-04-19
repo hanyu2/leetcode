f28e392678f7cfa8674dfdb16fe87ac39343ab43
Han <hanyu2@asu.edu>
2016-04-19 09:49:29 -0700

    129

--- Tree/SumRootToLeafNumbers.java
+++ Tree/SumRootToLeafNumbers.java
@@ -2,6 +2,7 @@ package Tree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class SumRootToLeafNumbers {
 	//My solution
@@ -43,5 +44,35 @@ public class SumRootToLeafNumbers {
         if (n == null) return 0;
         if (n.right == null && n.left == null) return s*10 + n.val;
         return sum(n.left, s*10 + n.val) + sum(n.right, s*10 + n.val);
+    }
+    
+    //Iterative
+    public int sumNumbers3(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        int sum = 0;
+        TreeNode curr;
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+
+        while(!stack.empty()){
+            curr = stack.pop();
+
+            if(curr.right!=null){
+                curr.right.val = curr.val*10+curr.right.val;
+                stack.push(curr.right);
+            }
+
+            if(curr.left!=null){
+                curr.left.val = curr.val*10+curr.left.val;
+                stack.push(curr.left);
+            }
+
+            if(curr.left==null && curr.right==null){ // leaf node
+                sum+=curr.val;
+            }
+        }
+        return sum;
     }
 }
