b421f62908c28b1ffb950963322fe28394108a8a
Han <hanyu2@asu.edu>
2016-05-05 13:18:32 -0700

    27

--- Array/RemoveElements.java
+++ Array/RemoveElements.java
@@ -14,8 +14,26 @@ public class RemoveElements {
 		}
 		return pointer + 1;
 	}
+	
+	public static int removeElement2(int[] nums, int val) {
+        int start = 0;
+        int end = nums.length - 1;
+        while(start <= end){
+            while(start < nums.length && nums[start] != val ){
+                start++;
+            }
+            while(end >= 0 && nums[end] == val ){
+                end--;
+            }
+            nums[start] = nums[end];
+            start++;
+            end--;
+        }
+        return start;
+    }
 	public static void main(String[] args) {
-		int [] nums = {5,2,4,5,8,1,6,0,4};
-		System.out.println(removeElement(nums,4));
+		//int [] nums = {5,2,4,5,8,1,6,0,4};
+		int [] nums = {1};
+		System.out.println(removeElement2(nums,1));
 	}
 }
