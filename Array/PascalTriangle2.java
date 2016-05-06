d2bd4f4350de197bb4e6e79d785e9137b497edda
Han <hanyu2@asu.edu>
2016-05-06 14:49:04 -0700

    119

--- Array/PascalTriangle2.java
+++ Array/PascalTriangle2.java
@@ -1,6 +1,7 @@
 package Array;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class PascalTriangle2 {
@@ -25,9 +26,35 @@ public class PascalTriangle2 {
 		}
 		return list;
 	}
+	//better
+	public static List<Integer> getRow2(int rowIndex) {
+        Integer[] arr = new Integer[rowIndex + 1];
+        Arrays.fill(arr, 0);
+        arr[0] = 1;
+
+        for (int i = 1; i <= rowIndex; i++) 
+            for (int j = i; j > 0; j--) 
+                arr[j] = arr[j] + arr[j - 1];
+
+        return Arrays.asList(arr);
+    }
+	//not efficient
+	public List<Integer> getRow3(int rowIndex) {
+	    List<Integer> list = new ArrayList<Integer>();
+	    if (rowIndex < 0)
+	        return list;
+
+	    for (int i = 0; i < rowIndex + 1; i++) {
+	        list.add(0, 1);//not effiecient
+	        for (int j = 1; j < list.size() - 1; j++) {
+	            list.set(j, list.get(j) + list.get(j + 1));
+	        }
+	    }
+	    return list;
+	}
 	
 	public static void main(String[] args) {
-		List<Integer> list = getRow(5);
+		List<Integer> list = getRow2(3);
 		for(int i : list){
 			System.out.print(i + " ");
 		}
