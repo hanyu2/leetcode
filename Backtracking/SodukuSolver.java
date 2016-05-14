f754a03260956d17081320c81686e89dd37ccb3f
Han <hanyu2@asu.edu>
2016-05-14 15:25:45 -0700

    76

--- Backtracking/SodukuSolver.java
+++ Backtracking/SodukuSolver.java
@@ -1,5 +1,83 @@
 package Backtracking;
 
-public class SodukuSolver {
+import java.util.Arrays;
 
+public class SodukuSolver {
+	public void solveSudoku(char[][] board) {
+		if (board == null || board.length == 0)
+			return;
+		solve(board);
+	}
+
+	public boolean solve(char[][] board) {
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board[0].length; j++) {
+				if (board[i][j] == '.') {
+					for (char c = '1'; c <= '9'; c++) {// trial. Try 1 through 9
+														// for each cell
+						if (isValid(board, i, j, c)) {
+							board[i][j] = c; // Put c for this cell
+
+							if (solve(board))
+								return true; // If it's the solution return true
+							else
+								board[i][j] = '.'; // Otherwise go back
+						}
+					}
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	public boolean isValid(char[][] board, int i, int j, char c) {
+		// Check colum
+		for (int row = 0; row < 9; row++)
+			if (board[row][j] == c)
+				return false;
+
+		// Check row
+		for (int col = 0; col < 9; col++)
+			if (board[i][col] == c)
+				return false;
+
+		// Check 3 x 3 block
+		for (int row = (i / 3) * 3; row < (i / 3) * 3 + 3; row++)
+			for (int col = (j / 3) * 3; col < (j / 3) * 3 + 3; col++)
+				if (board[row][col] == c)
+					return false;
+		return true;
+	}
+	
+	//DFS
+	public void solveSudoku2(char[][] board) {
+        dfs(board,0);
+    }
+    private boolean dfs(char[][] board, int d) {
+        if (d==81) return true; //found solution
+        int i=d/9, j=d%9;
+        if (board[i][j]!='.') return dfs(board,d+1);//prefill number skip
+
+        boolean[] flag=new boolean[10];
+        validate(board,i,j,flag);
+        for (int k=1; k<=9; k++) {
+            if (flag[k]) {
+                board[i][j]=(char)('0'+k);
+                if (dfs(board,d+1)) return true;
+            }
+        }
+        board[i][j]='.'; //if can not solve, in the wrong path, change back to '.' and out
+        return false;
+    }
+    private void validate(char[][] board, int i, int j, boolean[] flag) {
+        Arrays.fill(flag,true);
+        for (int k=0; k<9; k++) {
+            if (board[i][k]!='.') flag[board[i][k]-'0']=false;
+            if (board[k][j]!='.') flag[board[k][j]-'0']=false;
+            int r=i/3*3+k/3;
+            int c=j/3*3+k%3;
+            if (board[r][c]!='.') flag[board[r][c]-'0']=false;
+        }
+    }
 }
---
+++ Hashtable/MinimumWindowSubstring.java
@@ -0,0 +1,58 @@
+package Hashtable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MinimumWindowSubstring {
+	
+	public static String minWindow(String s, String t) {
+		Map<Character, Integer> map = new HashMap<Character, Integer>();
+		for (int i = 0; i < t.length(); i++) {
+			char c = t.charAt(i);
+			if (map.containsKey(c)) {
+				map.put(c, map.get(c) + 1);
+			} else {
+				map.put(c, 1);
+			}
+		}
+		int start = 0, end = 0;
+		int count = t.length();
+		int head = 0;
+		int d = Integer.MAX_VALUE;
+		while (end < s.length()) {
+			char c = s.charAt(end);
+			if (map.containsKey(c)) {
+				map.put(c, map.get(c) - 1);
+				if(map.get(c) >= 0){
+					count--;
+				}
+			}
+			end++;
+			while (count == 0) {
+				if (end - start < d) {
+					d = end - start;
+					head = start;
+				}
+				if (map.containsKey(s.charAt(start))) {
+					map.put(s.charAt(start), map.get(s.charAt(start)) + 1);
+					if (map.get(s.charAt(start)) > 0) {	
+						count++;
+					}
+				}
+				start++;
+			}
+		}
+		return d == Integer.MAX_VALUE ? "" : s.substring(head, head + d);
+	}
+
+	public static void main(String[] args) {
+		// System.out.println(minWindow("a", "aa"));
+		System.out.println(minWindow("a", "a"));
+		// System.out.println(minWindow("abc", "ab"));
+		// System.out.println(minWindow("ab", "b"));
+		//System.out.println(minWindow2("bba", "ab"));
+		// System.out.println(minWindow2("of_characters_and_as", "aas"));
+		// System.out.println(minWindow("acbbaca", "aba"));
+		// System.out.println(minWindow("ABABBbbBbBB", "BaAA"));
+	}
+}
