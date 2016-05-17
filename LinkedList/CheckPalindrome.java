24d8da16ae3e9e6038b304ae63d97a6262b4c302
Han <hanyu2@asu.edu>
2016-05-16 17:15:42 -0700

    92

--- LinkedList/CheckPalindrome.java
+++ LinkedList/CheckPalindrome.java
@@ -95,11 +95,13 @@ public class CheckPalindrome {
 	public static void main(String[] args) {
 		ListNode n1 = new ListNode(1);
 		ListNode n2 = new ListNode(2);
-		ListNode n3 = new ListNode(2);
-		ListNode n4 = new ListNode(1);
+		ListNode n3 = new ListNode(3);
+		ListNode n4 = new ListNode(2);
+		ListNode n5 = new ListNode(1);
 		n1.next = n2;
 		n2.next = n3;
 		n3.next = n4;
-		System.out.println(isPalindrome3(n1));
+		n4.next = n5;
+		System.out.println(isPalindrome2(n1));
 	}
 }
--- LinkedList/ReverseLinkedList2.java
+++ LinkedList/ReverseLinkedList2.java
@@ -1,7 +1,7 @@
 package LinkedList;
 
 public class ReverseLinkedList2 {
-	public ListNode reverseBetween(ListNode head, int m, int n) {
+	public static ListNode reverseBetween(ListNode head, int m, int n) {
         if(head == null || head.next == null){
             return head;
         }   
@@ -30,4 +30,18 @@ public class ReverseLinkedList2 {
         }
         return dummy.next;
     }
+	
+    public static void main(String[] args) {
+		ListNode n1 = new ListNode(1);
+		ListNode n2 = new ListNode(2);
+		ListNode n3 = new ListNode(3);
+		ListNode n4 = new ListNode(4);
+		ListNode n5 = new ListNode(5);
+
+		n1.next = n2;
+		n2.next = n3;
+		n3.next = n4;
+		n4.next = n5;
+		reverseBetween(n1, 2, 4);
+	}
 }
