4d85369e69e1351bf7c2e9a831873b0685888bc3
Han <hanyu2@asu.edu>
2016-05-16 17:49:18 -0700

    2

--- LinkedList/ConvertSortedListToBST.java
+++ LinkedList/ConvertSortedListToBST.java
@@ -6,91 +6,91 @@ public class ConvertSortedListToBST {
 	private ListNode node;
 
 	public TreeNode sortedListToBST(ListNode head) {
-	    if(head == null){
-	        return null;
-	    }
+		if (head == null) {
+			return null;
+		}
 
-	    int size = 0;
-	    ListNode runner = head;
-	    node = head;
+		int size = 0;
+		ListNode runner = head;
+		node = head;
 
-	    while(runner != null){
-	        runner = runner.next;
-	        size ++;
-	    }
+		while (runner != null) {
+			runner = runner.next;
+			size++;
+		}
 
-	    return inorderHelper(0, size - 1);
+		return inorderHelper(0, size - 1);
 	}
 
-	public TreeNode inorderHelper(int start, int end){
-	    if(start > end){
-	        return null;
-	    }
+	public TreeNode inorderHelper(int start, int end) {
+		if (start > end) {
+			return null;
+		}
 
-	    int mid = start + (end - start) / 2;
-	    TreeNode left = inorderHelper(start, mid - 1);
+		int mid = start + (end - start) / 2;
+		TreeNode left = inorderHelper(start, mid - 1);
 
-	    TreeNode treenode = new TreeNode(node.val);
-	    treenode.left = left;
-	    node = node.next;
+		TreeNode treenode = new TreeNode(node.val);
+		treenode.left = left;
+		node = node.next;
 
-	    TreeNode right = inorderHelper(mid + 1, end);
-	    treenode.right = right;
+		TreeNode right = inorderHelper(mid + 1, end);
+		treenode.right = right;
 
-	    return treenode;
+		return treenode;
 	}
-	//Recursion
+
+	// Recursion
 	public TreeNode sortedListToBST2(ListNode head) {
-	    if(head == null)
-	        return null;
-	    ListNode fast = head;
-	    ListNode slow = head;
-	    ListNode prev =null; 
-	    while(fast != null && fast.next != null)
-	    {
-	        fast = fast.next.next;
-	        prev =slow;
-	        slow=slow.next;
-	    }
-	    TreeNode root = new TreeNode(slow.val);
-	    if(prev != null)
-	        prev.next = null;
-	    else
-	        head  = null;
+		if (head == null)
+			return null;
+		ListNode fast = head;
+		ListNode slow = head;
+		ListNode prev = null;
+		while (fast != null && fast.next != null) {
+			fast = fast.next.next;
+			prev = slow;
+			slow = slow.next;
+		}
+		TreeNode root = new TreeNode(slow.val);
+		if (prev != null)
+			prev.next = null;
+		else
+			head = null;
 
-	    root.left = sortedListToBST(head);
-	    root.right = sortedListToBST(slow.next);
-	    return root;
+		root.left = sortedListToBST(head);
+		root.right = sortedListToBST(slow.next);
+		return root;
 	}
-	
+
 	public TreeNode sortedListToBST3(ListNode head) {
-        if(head==null){
-            return null;
-        }
-        if(head.next==null){
-            TreeNode treeNode=new TreeNode(head.val);
-            return treeNode;
-        }
-        ListNode slow=head;
-        ListNode fast=head.next.next;
-        while(fast!=null&&fast.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
-        }
-        TreeNode root=new TreeNode(slow.next.val);
-        ListNode temp=slow.next.next;
-        slow.next=null;
-        root.left=sortedListToBST(head);
-        root.right=sortedListToBST(temp);
-        return root;
-    }
+		if (head == null) {
+			return null;
+		}
+		if (head.next == null) {
+			TreeNode treeNode = new TreeNode(head.val);
+			return treeNode;
+		}
+		ListNode slow = head;
+		ListNode fast = head.next.next;
+		while (fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		TreeNode root = new TreeNode(slow.next.val);
+		ListNode temp = slow.next.next;
+		slow.next = null;
+		root.left = sortedListToBST(head);
+		root.right = sortedListToBST(temp);
+		return root;
+	}
 
-	/*public static void main(String[] args) {
-		ListNode n1 = new ListNode(1);
-		ListNode n2 = new ListNode(3);
-
-		n1.next = n2;
-		sortedListToBST(n1);
-
-	}*/
+	/*
+	 * public static void main(String[] args) { ListNode n1 = new ListNode(1);
+	 * ListNode n2 = new ListNode(3);
+	 * 
+	 * n1.next = n2; sortedListToBST(n1);
+	 * 
+	 * }
+	 */
 }
