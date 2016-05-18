eea04123bb9ce3af528255675d95e350d7bc6c9a
Han <hanyu2@asu.edu>
2016-05-18 11:48:02 -0700

    143

--- LinkedList/ReorderList.java
+++ LinkedList/ReorderList.java
@@ -7,35 +7,42 @@ public class ReorderList {
         }
         ListNode fast = head;
         ListNode slow = head;
-        while(fast.next != null && fast.next.next != null){
+        while(fast != null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
         }
-        ListNode cur = slow.next;
-        ListNode next = slow.next.next;
-        while(next != null){
-            ListNode temp = next.next;
-            next.next = slow.next;
-            slow.next = next;
-            cur.next = temp;
-            next = temp;
+        fast = slow.next;
+        slow.next = null;
+        while(fast != null){
+            ListNode next = fast.next;
+            fast.next = slow.next;
+            slow.next = fast;
+            fast = next;
         }
-        ListNode newHead = head; 
-        ListNode newCur = slow.next;
-        while(newHead != slow){
-        	slow.next = newCur.next;
-        	newCur.next = newHead.next;
-        	newHead.next = newCur;
-        	newHead = newCur.next;
-        	newCur = slow.next;
+        
+        fast = slow.next;
+        slow.next = null;
+        slow = head;
+        while(fast != null){
+            ListNode slowNext = slow.next;
+            ListNode fastNext = fast.next;
+            slow.next = fast;
+            fast.next = slowNext;
+            slow = slowNext;
+            fast = fastNext;
         }
     }
+	
 	public static void main(String[] args) {
 		ListNode n1 = new ListNode(1);
 		ListNode n2 = new ListNode(2);
-		//ListNode n3 = new ListNode(3);
+		ListNode n3 = new ListNode(3);
+		ListNode n4 = new ListNode(4);
+		ListNode n5 = new ListNode(5);
 		n1.next = n2;
-		//n2.next = n3;
+		n2.next = n3;
+		n3.next = n4;
+		n4.next = n5;
 		reorderList(n1);
 	}
 }
