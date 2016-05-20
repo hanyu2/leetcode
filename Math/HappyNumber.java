f808b3315002afdc9ecb0a736879894e04757763
Han <hanyu2@asu.edu>
2016-05-20 11:33:13 -0700

    202

--- Math/HappyNumber.java
+++ Math/HappyNumber.java
@@ -20,10 +20,33 @@ public class HappyNumber {
 		}
 		return true;
 	}
+	
+	public static boolean isHappy2(int n) {
+        int x = n;
+        int y = n;
+        while(x>1){
+            x = cal(x) ;
+            if(x==1) return true ;
+            y = cal(cal(y));
+            if(y==1) return true ;
+
+            if(x==y) return false;
+        }
+        return true ;
+    }
+    public static int cal(int n){
+        int x = n;
+        int s = 0;
+        while(x>0){
+            s = s+(x%10)*(x%10);
+            x = x/10;
+        }
+        return s ;
+    }
 
 	public static void main(String[] args) {
 		// System.out.println(isHappy(18));
-		System.out.println(isHappy(7));
+		System.out.println(isHappy2(7));
 		System.out.println(isHappy(1111111));
 	}
 }
