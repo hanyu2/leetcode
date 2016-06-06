88ae0dc530dcc4ac2fac791bae93ee37f199d251
Han <hanyu2@asu.edu>
2016-06-06 14:35:36 -0700

    282

---
+++ DFS/ExpressionAddOperators.java
@@ -0,0 +1,43 @@
+package DFS;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExpressionAddOperators {
+	public static List<String> addOperators(String num, int target) {
+		List<String> rst = new ArrayList<String>();
+		if (num == null || num.length() == 0)
+			return rst;
+		helper(rst, "", num, target, 0, 0, 0);
+		return rst;
+	}
+
+	public static void helper(List<String> rst, String path, String num, int target, int pos, long eval, long multed) {
+		if (pos == num.length()) {
+			if (target == eval)
+				rst.add(path);
+			return;
+		}
+		for (int i = pos; i < num.length(); i++) {
+			if (i != pos && num.charAt(pos) == '0')
+				break;
+			long cur = Long.parseLong(num.substring(pos, i + 1));
+			if (pos == 0) {
+				helper(rst, path + cur, num, target, i + 1, cur, cur);
+			} else {
+				helper(rst, path + "+" + cur, num, target, i + 1, eval + cur, cur);
+
+				helper(rst, path + "-" + cur, num, target, i + 1, eval - cur, -cur);
+
+				helper(rst, path + "*" + cur, num, target, i + 1, eval - multed + multed * cur, multed * cur);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		List<String> list = addOperators("155", 10);
+		for(String s : list){
+			System.out.println(s);
+		}
+	}
+}
