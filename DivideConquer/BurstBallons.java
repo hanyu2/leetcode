713fd1411817b41ed1fac6eaedf69a2c80cb27f4
Han <hanyu2@asu.edu>
2016-06-06 13:53:22 -0700

    312

--- DivideConquer/BurstBallons.java
+++ DivideConquer/BurstBallons.java
@@ -1,25 +1,38 @@
 package DivideConquer;
 
 public class BurstBallons {
-	public int maxCoins(int[] iNums) {
-	    int[] nums = new int[iNums.length + 2];
-	    int n = 1;
-	    for (int x : iNums) if (x > 0) nums[n++] = x;
-	    nums[0] = nums[n++] = 1;
+	//https://leetcode.com/discuss/72216/share-some-analysis-and-explanations
+	public static int maxCoins(int[] iNums) {
+		int[] nums = new int[iNums.length + 2];
+		int n = 1;
+		for (int x : iNums) {
+			if (x > 0) {
+				nums[n++] = x;
+			}
+		}
+		nums[0] = nums[n++] = 1;
 
-
-	    int[][] memo = new int[n][n];
-	    return burst(memo, nums, 0, n - 1);
+		int[][] memo = new int[n][n];
+		return burst(memo, nums, 0, n - 1);
 	}
 
-	public int burst(int[][] memo, int[] nums, int left, int right) {
-	    if (left + 1 == right) return 0;
-	    if (memo[left][right] > 0) return memo[left][right];
-	    int ans = 0;
-	    for (int i = left + 1; i < right; ++i)
-	        ans = Math.max(ans, nums[left] * nums[i] * nums[right] 
-	        + burst(memo, nums, left, i) + burst(memo, nums, i, right));
-	    memo[left][right] = ans;
-	    return ans;
+	public static int burst(int[][] memo, int[] nums, int left, int right) {
+		if (left + 1 == right)
+			return 0;
+		if (memo[left][right] > 0)
+			return memo[left][right];
+		int ans = 0;
+		for (int i = left + 1; i < right; ++i) {
+			int l = burst(memo, nums, left, i);
+			int r = burst(memo, nums, i, right);
+			ans = Math.max(ans, nums[left] * nums[i] * nums[right] + l + r);
+		}
+		memo[left][right] = ans;
+		return ans;
+	}
+
+	public static void main(String[] args) {
+		int[] nums = { 3, 1, 5, 8 };
+		maxCoins(nums);
 	}
 }
