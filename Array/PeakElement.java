41373892cbc4a29ec11a1226e47539fcd7de88ec
Han <hanyu2@asu.edu>
2016-06-08 17:06:49 -0700

    162

--- Array/PeakElement.java
+++ Array/PeakElement.java
@@ -22,16 +22,17 @@ public class PeakElement {
 	// Binary Search: iteration
 	public static int findPeakElement2(int[] nums) {
 		int low = 0;
-		int high = nums.length - 1;
-		while (low < high) {
-			int mid = (low + high) / 2;
-			if (nums[mid] < nums[mid + 1]) {
-				low = mid + 1;
-			} else {
-				high = mid;
-			}
-		}
-		return low;
+        int high = nums.length - 1;
+
+        while(low < high){
+            int mid1 = (low+high)/2;
+            int mid2 = mid1+1;
+            if(nums[mid1] < nums[mid2])
+                low = mid2;
+            else
+                high = mid1;
+        }
+        return low;
 	}
 
 	// Interation Sequential search O(N)git 
--- Array/Solution.java
+++ Array/Solution.java
@@ -5,48 +5,27 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
-	 public static int findKthLargest(int[] nums, int k) {
-	        k = nums.length - k;
+	 
+	 public static int minSubArrayLen(int s, int[] nums) {
+	        int [] array = new int[nums.length];
 	        int start = 0;
-	        int end = nums.length - 1;
-	        while(start < end){
-	            int pivot = partition(nums, start, end);
-	            if(pivot < k){
-	                start = pivot + 1;
-	            }else if(pivot > k){
-	                end = pivot - 1;
-	            }else{
-	                break;
+	        int sum = 0;
+	        int len = Integer.MAX_VALUE;
+	        for(int i = 0; i < nums.length; i++){
+	            sum += nums[i];
+	            if(sum == s){
+	                len = Math.min(len, i - start + 1);
+	            }else if(sum > s){
+	                sum -= nums[start++];
+	                sum -= nums[i];
+	                i--;
 	            }
 	        }
-	        return nums[k];
-	        
+	        return len == Integer.MAX_VALUE ? 0 : len;
 	    }
-	    public static int partition(int[] nums, int start, int end){
-	        int p = nums[end];
-	        int i = 0;
-	        int j = end - 1;
-	        while(i < j){
-	            while(i < j && nums[i] <= p){
-	                i++;
-	            }
-	            while(i < j && nums[j] >= p){
-	                j--;
-	            }
-	            swap(nums, i, j);
-	        }
-	        swap(nums, i, end);
-	        return i;
-	    }
-	    public static void swap(int[] nums, int i, int j){
-	        int temp = nums[i];
-	        nums[i] = nums[j];
-	        nums[j] = temp;
-	    }
-
+    
 	public static void main(String[] args) {
-		int[] nums = {3, 1, 2, 4};
-		Arrays.sort(nums);
-		findKthLargest(nums, 2);
+		int[] nums = {1,4, 4};
+		System.out.println(minSubArrayLen(4, nums));
 	}
 }
