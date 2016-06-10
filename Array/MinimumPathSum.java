784d64cdab9fd135696dac0fecda634084cb642a
Han <hanyu2@asu.edu>
2016-06-10 14:52:33 -0700

    64

--- Array/MinimumPathSum.java
+++ Array/MinimumPathSum.java
@@ -25,6 +25,27 @@ public class MinimumPathSum {
 		}
 		return grid[m - 1][n - 1];
 	}
+	//dp same idea as the above one
+	public int minPathSum3(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int [][] help = new int[m][n];
+        help[0][0] = grid[0][0];
+        for(int i = 1; i < m; i++){
+            help[i][0] = help[i - 1][0] + grid[i][0];
+        }
+        
+         for(int i = 1; i < n; i++){
+            help[0][i] = help[0][i - 1] + grid[0][i];
+        }
+        
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                help[i][j] = Math.min(grid[i][j] + help[i - 1][j], grid[i][j] + help[i][j - 1]);
+            }
+        }
+        return help[m - 1][n - 1];
+    }
 	// Dijkstra
 
 	static class PointComparator implements Comparator<int[]> {
