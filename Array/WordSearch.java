16aae00dcf163e057a38bc6e42b137b279d098d4
Han <hanyu2@asu.edu>
2016-06-21 14:36:19 -0700

    79

--- Array/WordSearch.java
+++ Array/WordSearch.java
@@ -1,34 +1,29 @@
 package Array;
 
 public class WordSearch {
-	public static boolean exist(char[][] board, String word) {
-		if(board == null || board.length == 0)
-            return false;
-        if(word.length() == 0)
-            return true;
-		char[] w = word.toCharArray();
-		for (int y = 0; y < board.length; y++) {
-			for (int x = 0; x < board[y].length; x++) {
-				if (existHelper(board, y, x, w, 0))
-					return true;
-			}
+	 public static boolean exist(char[][] board, String word) {
+		    char[] w = word.toCharArray();
+		    for (int y=0; y<board.length; y++) {
+		        for (int x=0; x<board[y].length; x++) {
+		            if (exist(board, y, x, w, 0)) return true;
+		        }
+		    }
+		    return false;
 		}
-		return false;
-	}
 
-	private static boolean existHelper(char[][] board, int y, int x, char[] word, int i) {
-		if (i == word.length)
-			return true;
-		if (y < 0 || x < 0 || y == board.length || x == board[y].length)//you cannot reverse the order of y == board.length and
-			return false;                                               //x == board[y].length because board[y] may not exist
-		if (board[y][x] != word[i])
-			return false;
-		board[y][x] = '*';
-		boolean exist = existHelper(board, y, x + 1, word, i + 1) || existHelper(board, y, x - 1, word, i + 1)
-				|| existHelper(board, y + 1, x, word, i + 1) || existHelper(board, y - 1, x, word, i + 1);
-		board[y][x] = word[i];
-		return exist;
-	}
+		private static boolean exist(char[][] board, int y, int x, char[] word, int i) {
+		    if (i == word.length) return true;
+		    if (y<0 || x<0 || y == board.length || x == board[y].length) return false;
+		    if (board[y][x] != word[i]) return false;
+		    board[y][x] = '*';
+		    boolean exist = exist(board, y, x+1, word, i+1)
+		        || exist(board, y, x-1, word, i+1)
+		        || exist(board, y+1, x, word, i+1)
+		        || exist(board, y-1, x, word, i+1);
+		    board[y][x] = word[i];
+		    return exist;
+		}
+
 
 	public static void main(String[] args) {
 		char board[][] = { { 'A', 'B', 'C', 'E' }, 
