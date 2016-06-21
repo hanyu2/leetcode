75541d5179bb28538b8a7e26d15c5e5a347dd3ea
Han <hanyu2@asu.edu>
2016-06-21 12:18:45 -0700

    44

---
+++ DP/WildcardMatching.java
@@ -0,0 +1,82 @@
+package DP;
+
+public class WildcardMatching {
+	/*e.g.
+
+	abed
+	?b*d**
+
+	a=?, go on, b=b, go on,
+	e=*, save * position star=3, save s position ss = 3, p++
+	e!=d,  check if there was a *, yes, ss++, s=ss; p=star+1
+	d=d, go on, meet the end.
+	check the rest element in p, if all are *, true, else false;
+*/
+	//http://yucoding.blogspot.com/2013/02/leetcode-question-123-wildcard-matching.html
+	public static boolean isMatch(String str, String pattern) {
+		int s = 0, p = 0, match = 0, starIdx = -1;
+		while (s < str.length()) {
+			// advancing both pointers
+			if (p < pattern.length() && (pattern.charAt(p) == '?' || str.charAt(s) == pattern.charAt(p))) {
+				s++;
+				p++;
+			}
+			// * found, only advancing pattern pointer
+			else if (p < pattern.length() && pattern.charAt(p) == '*') {
+				starIdx = p;
+				match = s;
+				p++;
+			}
+			// last pattern pointer was *, advancing string pointer
+			else if (starIdx != -1) {
+				p = starIdx + 1;
+				match++;
+				s = match;
+			}
+			// current pattern pointer is not star, last patter pointer was not
+			// *
+			// characters do not match
+			else
+				return false;
+		}
+
+		// check for remaining characters in pattern
+		while (p < pattern.length() && pattern.charAt(p) == '*')
+			p++;
+
+		return p == pattern.length();
+	}
+	
+	public static boolean isMatch2(String s, String p) {
+        int m = s.length(), n = p.length();
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (p.charAt(i) == '*') count++;
+        }
+        if (count==0 && m != n) return false;
+        else if (n - count > m) return false;
+
+        boolean[] match = new boolean[m+1];
+        match[0] = true;
+        for (int i = 0; i < m; i++) {
+            match[i+1] = false;
+        }
+        for (int i = 0; i < n; i++) {
+            if (p.charAt(i) == '*') {
+                for (int j = 0; j < m; j++) {
+                    match[j+1] = match[j] || match[j+1]; 
+                }
+            } else {
+                for (int j = m-1; j >= 0; j--) {
+                    match[j+1] = (p.charAt(i) == '?' || p.charAt(i) == s.charAt(j)) && match[j];
+                }
+                match[0] = false;
+            }
+        }
+        return match[m];
+    }
+	
+	public static void main(String[] args) {
+		System.out.println(isMatch2("abceeed", "ab*eed"));
+	}
+}
