368984f1d3ba2aab639a900819f40577d3269932
Han <hanyu2@asu.edu>
2016-06-23 14:50:27 -0700

    10

--- Backtracking/RegularExpression.java
+++ Backtracking/RegularExpression.java
@@ -1,52 +1,53 @@
 package Backtracking;
 
 public class RegularExpression {
-	public static boolean isMatch(String s, String p) {
-		if (p.isEmpty()) {
-			return s.isEmpty();
+	//http://www.cnblogs.com/lupx/p/leetcode-10.html
+	public boolean isMatch(String s, String p) {
+		if (p.length() == 0) {
+			return s.length() == 0;
 		}
 
-		if (p.length() == 1 || p.charAt(1) != '*') {
-			if (s.isEmpty() || (p.charAt(0) != '.' && p.charAt(0) != s.charAt(0))) {
-				return false;
-			} else {
-				return isMatch(s.substring(1), p.substring(1));
-			}
+		if (p.length() == 1) {
+			return s.length() == 1 && (p.charAt(0) == '.' || p.charAt(0) == s.charAt(0));
 		}
 
-		// P.length() >=2
-		while (!s.isEmpty() && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.')) {
+		if (p.charAt(1) == '*') {
 			if (isMatch(s, p.substring(2))) {
 				return true;
+			} else {
+				return s.length() > 0 && (p.charAt(0) == '.' || p.charAt(0) == s.charAt(0))
+						&& isMatch(s.substring(1), p);
 			}
-			s = s.substring(1);
+		} else {
+			return s.length() > 0 && (p.charAt(0) == '.' || p.charAt(0) == s.charAt(0))
+					&& isMatch(s.substring(1), p.substring(1));
 		}
-
-		return isMatch(s, p.substring(2));
 	}
-	//http://cwind.iteye.com/blog/2228723
-	public boolean isMatch2(String /* string to check */ s, String /* patterns */ p) {  
-        boolean[] match = new boolean[s.length()+1];  
-        for(int i=0; i<match.length; i++){  
-            match[i] = false;  
-        }  
-        match[s.length()] = true;  
-        for(int i=p.length()-1; i>=0; i--){  
-            if(p.charAt(i)=='*'){  
-                for(int j=s.length()-1; j>=0; j--){  
-                    match[j] = match[j]||match[j+1]&&(p.charAt(i-1)=='.'||p.charAt(i-1)==s.charAt(j));  
-                }  
-                i--;  
-            }else {  
-                for(int j=0; j<s.length(); j++){  
-                    match[j] = match[j+1]&&(p.charAt(i)=='.'||p.charAt(i)==s.charAt(j));  
-                }  
-                match[s.length()] = false;  
-            }  
-        }  
-        return match[0];  
-    }  
+
+	// http://cwind.iteye.com/blog/2228723
+	public static boolean isMatch2(String /* string to check */ s, String /* patterns */ p) {
+		boolean[] match = new boolean[s.length() + 1];
+		for (int i = 0; i < match.length; i++) {
+			match[i] = false;
+		}
+		match[s.length()] = true;
+		for (int i = p.length() - 1; i >= 0; i--) {
+			if (p.charAt(i) == '*') {
+				for (int j = s.length() - 1; j >= 0; j--) {
+					match[j] = match[j] || match[j + 1] && (p.charAt(i - 1) == '.' || p.charAt(i - 1) == s.charAt(j));
+				}
+				i--;
+			} else {
+				for (int j = 0; j < s.length(); j++) {
+					match[j] = match[j + 1] && (p.charAt(i) == '.' || p.charAt(i) == s.charAt(j));
+				}
+				match[s.length()] = false;
+			}
+		}
+		return match[0];
+	}
+
 	public static void main(String[] args) {
-		isMatch("aa","a");
+		isMatch2("aa", "a*");
 	}
 }
--- DP/WildcardMatching.java
+++ DP/WildcardMatching.java
@@ -1,18 +1,17 @@
 package DP;
 
 public class WildcardMatching {
-	/*e.g.
-
-	abed
-	?b*d**
-
-	a=?, go on, b=b, go on,
-	e=*, save * position star=3, save s position ss = 3, p++
-	e!=d,  check if there was a *, yes, ss++, s=ss; p=star+1
-	d=d, go on, meet the end.
-	check the rest element in p, if all are *, true, else false;
-*/
-	//http://yucoding.blogspot.com/2013/02/leetcode-question-123-wildcard-matching.html
+	/*
+	 * e.g.
+	 * 
+	 * abed ?b*d**
+	 * 
+	 * a=?, go on, b=b, go on, e=*, save * position star=3, save s position ss =
+	 * 3, p++ e!=d, check if there was a *, yes, ss++, s=ss; p=star+1 d=d, go
+	 * on, meet the end. check the rest element in p, if all are *, true, else
+	 * false;
+	 */
+	// http://yucoding.blogspot.com/2013/02/leetcode-question-123-wildcard-matching.html
 	public static boolean isMatch(String str, String pattern) {
 		int s = 0, p = 0, match = 0, starIdx = -1;
 		while (s < str.length()) {
@@ -46,37 +45,43 @@ public class WildcardMatching {
 
 		return p == pattern.length();
 	}
-	
-	public static boolean isMatch2(String s, String p) {
-        int m = s.length(), n = p.length();
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            if (p.charAt(i) == '*') count++;
-        }
-        if (count==0 && m != n) return false;
-        else if (n - count > m) return false;
 
-        boolean[] match = new boolean[m+1];
-        match[0] = true;
-        for (int i = 0; i < m; i++) {
-            match[i+1] = false;
-        }
-        for (int i = 0; i < n; i++) {
-            if (p.charAt(i) == '*') {
-                for (int j = 0; j < m; j++) {
-                    match[j+1] = match[j] || match[j+1]; 
-                }
-            } else {
-                for (int j = m-1; j >= 0; j--) {
-                    match[j+1] = (p.charAt(i) == '?' || p.charAt(i) == s.charAt(j)) && match[j];
-                }
-                match[0] = false;
-            }
-        }
-        return match[m];
-    }
-	
+	public static boolean isMatch2(String s, String p) {
+		int m = s.length(), n = p.length();
+		int count = 0;
+		for (int i = 0; i < n; i++) {
+			if (p.charAt(i) == '*')
+				count++;
+		}
+		if (count == 0 && m != n)
+			return false;
+		else if (n - count > m)
+			return false;
+
+		boolean[] match = new boolean[m + 1];
+		match[0] = true;
+		for (int i = 0; i < m; i++) {
+			match[i + 1] = false;
+		}
+		for (int i = 0; i < n; i++) {
+			if (p.charAt(i) == '*') {
+				for (int j = 0; j < m; j++) {
+					match[j + 1] = match[j] || match[j + 1];
+				}
+			} else {
+				for (int j = m - 1; j >= 0; j--) {
+					match[j + 1] = (p.charAt(i) == '?' || p.charAt(i) == s.charAt(j)) && match[j];
+				}
+				match[0] = false;
+			}
+		}
+		return match[m];
+	}
+
 	public static void main(String[] args) {
-		System.out.println(isMatch2("abceeed", "ab*eed"));
+		isMatch("ho", "ho**");
+		System.out.println(isMatch("c", "*?*"));
+		System.out.println(isMatch("aa", "a"));
+		System.out.println(isMatch("abceeed", "ab*eed"));
 	}
 }
