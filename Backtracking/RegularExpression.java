24edfd0e0f0e6b9d6c968a7460664ff0377636f1
Han <hanyu2@asu.edu>
2016-06-24 10:32:17 -0700

    155

--- Backtracking/RegularExpression.java
+++ Backtracking/RegularExpression.java
@@ -24,29 +24,76 @@ public class RegularExpression {
 		}
 	}
 
-	// http://cwind.iteye.com/blog/2228723
-	public static boolean isMatch2(String /* string to check */ s, String /* patterns */ p) {
-		boolean[] match = new boolean[s.length() + 1];
-		for (int i = 0; i < match.length; i++) {
-			match[i] = false;
-		}
-		match[s.length()] = true;
-		for (int i = p.length() - 1; i >= 0; i--) {
-			if (p.charAt(i) == '*') {
-				for (int j = s.length() - 1; j >= 0; j--) {
-					match[j] = match[j] || match[j + 1] && (p.charAt(i - 1) == '.' || p.charAt(i - 1) == s.charAt(j));
-				}
-				i--;
-			} else {
-				for (int j = 0; j < s.length(); j++) {
-					match[j] = match[j + 1] && (p.charAt(i) == '.' || p.charAt(i) == s.charAt(j));
-				}
-				match[s.length()] = false;
-			}
-		}
-		return match[0];
-	}
-
+	//http://www.cnblogs.com/lupx/p/leetcode-10.html
+	public static boolean isMatch2(String s, String p) {
+		          int slen = s.length();
+		         int plen = p.length();
+		 
+		         /**
+		          * 保存动态规划的中间结果,我们用dp[i][j]来表示: S{0,..i-1} 与P{0,..j-1}的匹配结果.
+		          */
+		         boolean dp[][] = new boolean[slen+1][plen+1];//上面解释了,i和j在dp里代表s和p的下标.所以,dp尺寸需要加1
+		 
+		         /**
+		         * 下面来分析一下递推公式(DP少不了这个东西!).
+		         * 所谓递推公式就是根据之前已经保存的状态推出当前的状态. 也即求当前dp[i][j],可根据之前的结果间接的求出
+		          * 假设当前求dp[i][j], 它代表了S{0->i-1}与P{0->j-1}的匹配情况. 那么有以下几个可能:
+		          * (1)如果p{j-1}当前不是*,情况简单,当前匹配的唯一条件就是p{j-1}要与s{i-1}匹配
+		          *    并且, 之前也都一直匹配, dp[i-1][j-1]匹配! 两者哪个不满足都是false,所以两个条件"&&"一下即可.
+		          *    得递推公式:
+		          *    when p{j-1}!='*', dp[i][j] = dp[i-1][j-1] && p{j-1} == s{i-1} || p{j-1} == '.'
+		        * (2)如果p{j-1}当前是个*, 情况比较复杂了. 首先看看有哪几种可能性, 我们设p{j-2} = X, X* 是个二元组
+		          *   (2.1) X没有在s中重复过, 也即X重复了0次, 所以这种情况就是只要dp[i][j-2]为true, 当前就可以为true.
+		          *   (2.2) X在S中...i-3,i-2,i-1的位置出现过>=1次, >=1可以拆分开理解,=1成立&&>1也成立!(这是本题最难的部分!一旦理解,这个题就是个easy题了!)
+		        *         那么可以假设出现一次的话, 显然必须满足 p{j-2}==s{i-1}||p{j-2}=='.'
+		          *         出现>1次, 还应要求, S{0->i-2}最起码要能匹配p{0->j-1}, 也即dp[i-1][j]也需为true
+		          *   综上, 2.1和2.2之间是或者的关系,但是2.2内部,>=1我们拆成了>1&&=1的情况,这样就是个&&的关系
+		          *    得递推公式:
+		          *    when p{j-1}=='*', dp[i][j] = dp[i][j-2] || (p{j-2}==s{i-1}||p{j-2}=='.') && dp[i-1][j]
+		          * 有了递推公式, 我们可以看到,当i和j分别推进到各自边界的时候,两个串的最终匹配结果一定保存在dp[slen][plen],return这个结果就可以了!
+		          */
+		 
+		         /**
+		         * 显然 dp[0][0] = true, 因为代表两个空串做匹配的结果,肯定是true
+		         */
+		         dp[0][0] = true;
+		 
+		         /**
+		          * 当p为空串的时候,s有字符,显然全部不可能匹配
+		         */
+		         for(int i = 1; i <= s.length(); i++) {
+		             dp[i][0] = false;
+		         }
+		 
+		         /**
+		          * 显然, i=0, j从1-plen遍历的各个结果,代表了p各个子串分别是否能否匹配空串s.
+		          * 有一定可能, 当p中j-1位置是*,并且0->j-3的匹配结果是true, 也即dp[0][j-2] = true
+		          * 否则,dp[0][j] =false
+		          * 这里, 我们把i=0的第一行计算出来
+		          */
+		         for(int j = 1; j <= p.length(); j++) {
+		             //之所以从1开始,是为了方便理解: j位置结果表示了p{0->j-1}的匹配结果
+		             //所以,显然dp[0][1]代表了p第一个字符是否能够匹配空串, 显然是不可能的
+		             if(j==1) dp[0][j] = false;
+		            else dp[0][j] = p.charAt(j-1) == '*' && dp[0][j-2];
+		         }
+		 
+		 
+		 
+		         /**到这里,我们就已经分析完了基本边界情况以及空串情况,下来开始递推*/
+		         for(int i = 1; i <= slen; i++) {
+		             for(int j = 1; j <= plen; j++) {
+		                 if(p.charAt(j-1) != '*') {
+		                     dp[i][j] = dp[i-1][j-1] && (p.charAt(j-1) == '.' || p.charAt(j-1) == s.charAt(i-1));
+		                 }
+		                 else {
+		                     dp[i][j] = dp[i][j-2]||
+		                             (p.charAt(j-2) == '.' || p.charAt(j-2) == s.charAt(i-1)) && dp[i-1][j];
+		                 }
+		             }
+		         }
+		         return dp[slen][plen];
+		     }
 	public static void main(String[] args) {
 		isMatch2("aa", "a*");
 	}
--- Stack/MinStack.java
+++ Stack/MinStack.java
@@ -3,54 +3,55 @@ package Stack;
 import java.util.Stack;
 
 public class MinStack {
-	 Stack<Integer> stack = new Stack<Integer>();
-		Stack<Integer> min = new Stack<Integer>();
-		int minv = Integer.MAX_VALUE;
+	// check out two one stack solutions another one is in the comment
+	// https://leetcode.com/discuss/15679/share-my-java-solution-with-only-one-stack
+	Stack<Integer> stack = new Stack<Integer>();
+	Stack<Integer> min = new Stack<Integer>();
+	int minv = Integer.MAX_VALUE;
 
-	   /*
-	    First version
-	    int minValue = Integer.MAX_VALUE;
-	    public void push(int x){
-	        if(x < minValue){ // this should be x <= minValue 
-	            minValue = x; //don't foget this and in while loops don't forget i++
-	            min.push(x);
-	        }
-	        stack.push(x)
-	    }
-	    this has a bug when min is empty and push a new element the min stack, right at this time minValue is the old value and cannot be updated so a new element cannot be added into the min stack
-	   */
-		public void push(int x) {
-			if (min.isEmpty() || min.peek() >= x) {  
-				min.push(x);
-			}
-			stack.push(x);
+	/*
+	 * First version int minValue = Integer.MAX_VALUE; public void push(int x){
+	 * if(x < minValue){ // this should be x <= minValue minValue = x; //don't
+	 * foget this and in while loops don't forget i++ min.push(x); }
+	 * stack.push(x) } this has a bug when min is empty and push a new element
+	 * the min stack, right at this time minValue is the old value and cannot be
+	 * updated so a new element cannot be added into the min stack
+	 */
+	public void push(int x) {
+		if (min.isEmpty() || min.peek() >= x) {
+			min.push(x);
 		}
+		stack.push(x);
+	}
 
-		public void pop() {
-			if (stack.isEmpty()) {
-				return;
-			}
-			int a = stack.peek();
-			int b = min.peek();
-			if (a == b) { //at first I wrote if(stack.peek() == min.peek()) this does not work 
-			    min.pop(); //becuase peek() returns Integer and you cannot use == to compare the value
-			}
-			stack.pop();
+	public void pop() {
+		if (stack.isEmpty()) {
+			return;
 		}
+		int a = stack.peek();
+		int b = min.peek();
+		if (a == b) { // at first I wrote if(stack.peek() == min.peek()) this
+						// does not work
+			min.pop(); // becuase peek() returns Integer and you cannot use ==
+						// to compare the value
+		}
+		stack.pop();
+	}
 
-		public int top() {
-			if (stack.isEmpty()) {
-				return 0;
-			}
-			return stack.peek();
+	public int top() {
+		if (stack.isEmpty()) {
+			return 0;
 		}
+		return stack.peek();
+	}
 
-		public int getMin() {
-			if (min.isEmpty()) {
-				return 0;
-			}
-			return min.peek();
+	public int getMin() {
+		if (min.isEmpty()) {
+			return 0;
 		}
+		return min.peek();
+	}
+
 	public static void main(String[] args) {
 		MinStack stack = new MinStack();
 		stack.push(2147483646);
