126303bde83717b72ecb052c8011c06ef17db335
Han <hanyu2@asu.edu>
2016-06-27 11:50:45 -0700

    103

--- Tree/ZigZagLevelOrder.java
+++ Tree/ZigZagLevelOrder.java
@@ -3,6 +3,7 @@ package Tree;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 public class ZigZagLevelOrder {
 	public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
@@ -28,5 +29,51 @@ public class ZigZagLevelOrder {
 
 		travel(curr.left, sol, level + 1);
 		travel(curr.right, sol, level + 1);
+	}
+	
+	public static List<List<Integer>> zigzagLevelOrder2(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if(root == null){
+            return res;
+        }
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        Stack<TreeNode> stack2 = new Stack<TreeNode>();
+        stack.push(root);
+        int level = 1;
+        while(!stack.isEmpty()){
+            List<Integer> list = new ArrayList<Integer>();
+            while(!stack.isEmpty()){
+                TreeNode node = stack.pop();
+                list.add(node.val);
+                if(level % 2 == 1){
+                    if(node.left != null){
+                        stack2.push(node.left);
+                    }
+                    if(node.right != null){
+                        stack2.push(node.right);
+                    }
+                }else{
+                    if(node.right != null){
+                        stack2.push(node.right);
+                    }
+                    if(node.left != null){
+                        stack2.push(node.left);
+                    }
+                }
+            }
+            level++;
+            res.add(list);
+            stack.addAll(stack2);
+            stack2.clear();
+        }
+        return res;
+    }
+	public static void main(String[] args) {
+		TreeNode n1 = new TreeNode(3);
+		TreeNode n2 = new TreeNode(9);
+		TreeNode n3 = new TreeNode(20);
+		n1.left = n2;
+		n1.right = n3;
+		zigzagLevelOrder2(n1);
 	}
 }
