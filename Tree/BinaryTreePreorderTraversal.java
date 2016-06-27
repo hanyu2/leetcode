cd50e1ac83f24fc6822d610165913d98bf5f3d24
Han <hanyu2@asu.edu>
2016-06-27 12:42:43 -0700

    144

--- Tree/BinaryTreePreorderTraversal.java
+++ Tree/BinaryTreePreorderTraversal.java
@@ -1,6 +1,7 @@
 package Tree;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
 
@@ -36,4 +37,17 @@ public class BinaryTreePreorderTraversal {
 		}
 		return res;
 	}
+	//stack
+	public List<Integer> preorderTraversal3(TreeNode root) {
+        List<Integer> list = new LinkedList<Integer>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        if(root!=null) stack.push(root);
+        while(!stack.isEmpty()){
+            TreeNode node = stack.pop();
+            list.add(node.val);
+            if(node.right!=null) stack.push(node.right);
+            if(node.left!=null) stack.push(node.left);
+        }
+        return list;
+    }
 }
