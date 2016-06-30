168682f200a6f3e0e754a0a0e67e4970d13f7472
Han <hanyu2@asu.edu>
2016-06-30 10:50:51 -0700

    226

--- Tree/InvertTree.java
+++ Tree/InvertTree.java
@@ -1,28 +1,34 @@
 package Tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class InvertTree {
 	public static TreeNode invertTree(TreeNode root) {
-        if(root == null){
-            return null;
+		if(root == null){
+            return root;
         }
-        invert(root);
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.left = right;
+        root.right = left;
         return root;
     }
-    
-    public static void invert(TreeNode root){
-        if(root.left == null && root.right == null){
-            return;
-        }
-        TreeNode temp = root.left;
-        root.left = root.right;
-        root.right = temp;
-        if(root.left != null){
-        	invert(root.left);
-        }
-        if(root.right != null){
-        	invert(root.right);
-        }
-    }
+	
+	public TreeNode invertTree2(TreeNode root) {
+	    if (root == null) return null;
+	    Queue<TreeNode> queue = new LinkedList<TreeNode>();
+	    queue.add(root);
+	    while (!queue.isEmpty()) {
+	        TreeNode current = queue.poll();
+	        TreeNode temp = current.left;
+	        current.left = current.right;
+	        current.right = temp;
+	        if (current.left != null) queue.add(current.left);
+	        if (current.right != null) queue.add(current.right);
+	    }
+	    return root;
+	}
     public static void main(String[] args) {
     	TreeNode n1 = new TreeNode(1);
 		TreeNode n2 = new TreeNode(2);
