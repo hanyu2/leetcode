c85337ffe8e64674d4655c3865050e268b01a74d
Han <hanyu2@asu.edu>
2016-06-30 11:28:29 -0700

    112

--- Tree/PathSum.java
+++ Tree/PathSum.java
@@ -1,16 +1,18 @@
 package Tree;
 
+import java.util.Stack;
+
 public class PathSum {
-	public static boolean hasPathSum(TreeNode root, int sum){
-		if(root == null){
+	public static boolean hasPathSum(TreeNode root, int sum) {
+		if (root == null) {
 			return false;
 		}
-		if(root.left == null && root.right == null){
+		if (root.left == null && root.right == null) {
 			return sum - root.val == 0;
 		}
 		return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
 	}
-	
+
 	public static boolean hasPathSum2(TreeNode root, int sum) {
 		return check(root, 0, sum);
 	}
@@ -19,10 +21,39 @@ public class PathSum {
 		if (root == null) {
 			return false;
 		}
-		if(root.left == null && root.right == null){
+		if (root.left == null && root.right == null) {
 			return result + root.val == sum;
 		}
 		return check(root.left, result + root.val, sum) || check(root.right, result + root.val, sum);
+	}
+	//iterative
+	public boolean hasPathSum3(TreeNode root, int sum) {
+		if (root == null) {
+			return false;
+		}
+		Stack<TreeNode> path = new Stack<>();
+		Stack<Integer> sub = new Stack<>();
+		path.push(root);
+		sub.push(root.val);
+		while (!path.isEmpty()) {
+			TreeNode temp = path.pop();
+			int tempVal = sub.pop();
+			if (temp.left == null && temp.right == null) {
+				if (tempVal == sum){
+					return true;
+				}
+			} else {
+				if (temp.left != null) {
+					path.push(temp.left);
+					sub.push(temp.left.val + tempVal);
+				}
+				if (temp.right != null) {
+					path.push(temp.right);
+					sub.push(temp.right.val + tempVal);
+				}
+			}
+		}
+		return false;
 	}
 
 	public static void main(String[] args) {
