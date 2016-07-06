9562f7da88b54123118a2b9bf80c02c28a0f7f94
Han <hanyu2@asu.edu>
2016-07-06 16:43:28 -0700

    263

--- Math/UglyNum.java
+++ Math/UglyNum.java
@@ -2,27 +2,12 @@ package Math;
 
 public class UglyNum {
 	public static boolean isUgly(int num) {
-		if(num < 2){
-			return true;
-		}
-		while(num != 2 && num != 3 && num != 5){
-			if(num % 2 == 0){
-				num = num / 2;
-				continue;
-			}
-			if(num % 3 == 0){
-				num = num /3;
-				continue;
-			}
-			if(num % 5 == 0){
-				num = num / 5;
-				continue;
-			}
-			if((num % 2 != 0)&&(num % 3 != 0)&&(num % 5 != 0)){
-				return false;
-			}
-		}
-		return true;
+		 if(num==1) return true;
+		    if(num==0) return false;
+			while(num%2==0) num=num>>1;
+			while(num%3==0) num=num/3;
+			while(num%5==0) num=num/5;
+		    return num==1;
 	}
 	public static void main(String[] args) {
 		System.out.println(isUgly(6));
