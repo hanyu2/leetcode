e2491c9ba5a4eb72f5dfd51a902983461384c856
Han <hanyu2@asu.edu>
2016-07-09 16:19:06 -0700

    316

--- Greedy/RemoveDuplicateLetters.java
+++ Greedy/RemoveDuplicateLetters.java
@@ -42,7 +42,19 @@ public class RemoveDuplicateLetters {
 		return sb.toString();
 	}
 	
+	//greedy
+	public static String removeDuplicateLetters2(String s) {
+        int[] cnt = new int[26];
+        int pos = 0; // the position for the smallest s[i]
+        for (int i = 0; i < s.length(); i++) cnt[s.charAt(i) - 'a']++;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) < s.charAt(pos)) pos = i;
+            if (--cnt[s.charAt(i) - 'a'] == 0) break;
+        }
+        return s.length() == 0 ? "" : s.charAt(pos) + removeDuplicateLetters(s.substring(pos + 1).replaceAll("" + s.charAt(pos), ""));
+    }
+	
 	public static void main(String[] args) {
-		removeDuplicateLetters("bcabc");
+		System.out.println(removeDuplicateLetters2("bbcaac"));
 	}
 }
