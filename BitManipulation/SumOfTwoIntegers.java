c72660c7c969807ee583c021ee43310cca4f5eec
Han <hanyu2@asu.edu>
2016-07-24 18:43:22 -0700

    371

---
+++ BitManipulation/SumOfTwoIntegers.java
@@ -0,0 +1,44 @@
+package BitManipulation;
+
+public class SumOfTwoIntegers {
+	// Iterative
+	//http://blog.csdn.net/zhongjiekangping/article/details/6855864
+	public int getSum(int a, int b) {
+		if (a == 0) return b;
+		if (b == 0) return a;
+
+		while (b != 0) {
+			int carry = a & b;
+			a = a ^ b;
+			b = carry << 1;
+		}
+		
+		return a;
+	}
+
+	// Iterative
+	public int getSubtract(int a, int b) {
+		while (b != 0) {
+			int borrow = (~a) & b;
+			a = a ^ b;
+			b = borrow << 1;
+		}
+		
+		return a;
+	}
+
+	// Recursive
+	public int getSum2(int a, int b) {
+		return (b == 0) ? a : getSum(a ^ b, (a & b) << 1);
+	}
+
+	// Recursive
+	public int getSubtract2(int a, int b) {
+		return (b == 0) ? a : getSubtract(a ^ b, (~a & b) << 1);
+	}
+
+	// Get negative number
+	public int negate(int x) {
+		return ~x + 1;
+	}
+}
