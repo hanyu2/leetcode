92b9c6969932073d29eb86a6da3f15a4c2595d73
Han <hanyu2@asu.edu>
2016-07-25 11:40:55 -0700

    260

--- BitManipulation/SingleNumber3.java
+++ BitManipulation/SingleNumber3.java
@@ -1,34 +1,35 @@
 package BitManipulation;
 
 public class SingleNumber3 {
-	//https://leetcode.com/discuss/52351/accepted-java-space-easy-solution-with-detail-explanations
+	// https://leetcode.com/discuss/52351/accepted-java-space-easy-solution-with-detail-explanations
+	//https://discuss.leetcode.com/topic/25382/sharing-explanation-of-the-solution/2
 	public static int[] singleNumber(int[] nums) {
-        // Pass 1 : 
-        // Get the XOR of the two numbers we need to find
-        int diff = 0;
-        for (int num : nums) {
-            diff ^= num;
-        }
-        // Get its last set bit
-        diff &= -diff;
+		// Pass 1 :
+		// Get the XOR of the two numbers we need to find
+		int diff = 0;
+		for (int num : nums) {
+			diff ^= num;
+		}
+		// Get its last set bit
+		diff &= -diff;
 
-        // Pass 2 :
-        int[] rets = {0, 0}; // this array stores the two numbers we will return
-        for (int num : nums)
-        {
-            if ((num & diff) == 0) // the bit is not set
-            {
-                rets[0] ^= num;
-            }
-            else // the bit is set
-            {
-                rets[1] ^= num;
-            }
-        }
-        return rets;
-    }
+		// Pass 2 :
+		int[] rets = { 0, 0 }; // this array stores the two numbers we will
+								// return
+		for (int num : nums) {
+			if ((num & diff) == 0) // the bit is not set
+			{
+				rets[0] ^= num;
+			} else // the bit is set
+			{
+				rets[1] ^= num;
+			}
+		}
+		return rets;
+	}
+
 	public static void main(String[] args) {
-		int[] nums = {1, 1, 3, 2, 5, 2};
+		int[] nums = { 1, 1, 3, 2, 5, 2 };
 		singleNumber(nums);
 	}
 }
