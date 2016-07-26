e94aaed3d634cf9034cc5198e8048179168a66a2
Han <hanyu2@asu.edu>
2016-07-25 17:48:15 -0700

    336

--- Hashtable/PalindromePairs.java
+++ Hashtable/PalindromePairs.java
@@ -1,6 +1,7 @@
 package Hashtable;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -58,8 +59,79 @@ public class PalindromePairs {
 		return true;
 	}
 	
+	static	class TrieNode {
+		TrieNode[] next;
+		int index;
+		List<Integer> list;
+		
+		TrieNode() {
+			next = new TrieNode[26];
+			index = -1;
+			list = new ArrayList<>();
+		}
+	}
+
+	public static List<List<Integer>> palindromePairs2(String[] words) {
+	    List<List<Integer>> res = new ArrayList<>();
+	    
+	    TrieNode root = new TrieNode();
+	    
+	    for (int i = 0; i < words.length; i++) {
+			addWord(root, words[i], i);
+		}
+	    
+	    for (int i = 0; i < words.length; i++) {
+	    	search(words, i, root, res);
+	    }
+	    
+	    return res;
+	}
+
+	private static void addWord(TrieNode root, String word, int index) {
+		for (int i = word.length() - 1; i >= 0; i--) {
+			if (root.next[word.charAt(i) - 'a'] == null) {
+				root.next[word.charAt(i) - 'a'] = new TrieNode();
+			}
+			
+			if (isPalindrome(word, 0, i)) {
+				root.list.add(index);
+			}
+			
+			root = root.next[word.charAt(i) - 'a'];
+		}
+		
+		root.list.add(index);
+		root.index = index;
+	}
+
+	private static void search(String[] words, int i, TrieNode root, List<List<Integer>> list) {
+		for (int j = 0; j < words[i].length(); j++) {	
+			if (root.index >= 0 && root.index != i && isPalindrome(words[i], j, words[i].length() - 1)) {
+				list.add(Arrays.asList(i, root.index));
+			}
+			
+			root = root.next[words[i].charAt(j) - 'a'];
+	  		if (root == null) return;
+		}
+		
+		for (int j : root.list) {
+			if (i == j) continue;
+			list.add(Arrays.asList(i, j));
+		}
+	}
+
+	private static boolean isPalindrome(String word, int i, int j) {
+		while (i < j) {
+			if (word.charAt(i++) != word.charAt(j--)) return false;
+		}
+		
+		return true;
+	}
+	
+	
+	
 	public static void main(String[] args) {
 		String [] words = {"bat", "tab", "cat"};
-		palindromePairs(words);
+		palindromePairs2(words);
 	}
 }
