6bc1c262de925437d114830a7116c7554c3724e3
Han <hanyu2@asu.edu>
2016-07-28 17:24:22 -0700

    BinaryIndexedTree

--- SegmentTree/NumArray.java
+++ SegmentTree/NumArray.java
@@ -15,14 +15,14 @@ public class NumArray {
 		}
 	}
 
-	SegmentTreeNode root = null;
+	static SegmentTreeNode root = null;
 
 	public NumArray(int[] nums) {
 		root = buildTree(nums, 0, nums.length - 1);
 	}
 
 	private SegmentTreeNode buildTree(int[] nums, int start, int end) {
-		if (start > end) {
+		if (start > end) {// prevent num.length == 0
 			return null;
 		} else {
 			SegmentTreeNode node = new SegmentTreeNode(start, end);
@@ -38,11 +38,11 @@ public class NumArray {
 		}
 	}
 
-	void update(int i, int val) {
+	static void update(int i, int val) {
 		update(root, i, val);
 	}
 
-	void update(SegmentTreeNode root, int pos, int val) {
+	static void update(SegmentTreeNode root, int pos, int val) {
 		if (root.start == root.end) {
 			root.sum = val;
 		} else {
@@ -56,11 +56,11 @@ public class NumArray {
 		}
 	}
 
-	public int sumRange(int i, int j) {
+	static public int sumRange(int i, int j) {
 		return sumRange(root, i, j);
 	}
 
-	public int sumRange(SegmentTreeNode root, int start, int end) {
+	static public int sumRange(SegmentTreeNode root, int start, int end) {
 		if (root.end == end && root.start == start) {
 			return root.sum;
 		} else {
@@ -74,59 +74,69 @@ public class NumArray {
 			}
 		}
 	}
-	
-	
-	//Array implementation
+
+	// Array implementation
+	//BIT
 	int[] tree;
 	int n;
-	/*public NumArray2(int[] nums) {
-	    if (nums.length > 0) {
-	        n = nums.length;
-	        tree = new int[n * 2];
-	        buildTree2(nums);
-	    }
-	}*/
+
+	/*
+	 * public NumArray2(int[] nums) { 
+	 *    if (nums.length > 0) { 
+	 * 		n = nums.length;
+	 * 		tree = new int[n * 2]; 
+	 * 		buildTree2(nums); 
+	 *    } 
+	 * }
+	 */
 	private void buildTree2(int[] nums) {
-	    for (int i = n, j = 0;  i < 2 * n; i++,  j++)
-	        tree[i] = nums[j];
-	    for (int i = n - 1; i > 0; --i)
-	        tree[i] = tree[i * 2] + tree[i * 2 + 1];
+		for (int i = n, j = 0; i < 2 * n; i++, j++)
+			tree[i] = nums[j];
+		for (int i = n - 1; i > 0; --i)
+			tree[i] = tree[i * 2] + tree[i * 2 + 1];
 	}
 
 	void update2(int pos, int val) {
-	    pos += n;
-	    tree[pos] = val;
-	    while (pos > 0) {
-	        int left = pos;
-	        int right = pos;
-	        if (pos % 2 == 0) {
-	            right = pos + 1;
-	        } else {
-	            left = pos - 1;
-	        }
-	        // parent is updated after child is updated
-	        tree[pos / 2] = tree[left] + tree[right];
-	        pos /= 2;
-	    }
+		pos += n;
+		tree[pos] = val;
+		while (pos > 0) {
+			int left = pos;
+			int right = pos;
+			if (pos % 2 == 0) {
+				right = pos + 1;
+			} else {
+				left = pos - 1;
+			}
+			// parent is updated after child is updated
+			tree[pos / 2] = tree[left] + tree[right];
+			pos /= 2;
+		}
 	}
+
 	public int sumRange2(int l, int r) {
-	    // get leaf with value 'l'
-	    l += n;
-	    // get leaf with value 'r'
-	    r += n;
-	    int sum = 0;
-	    while (l <= r) {
-	        if ((l % 2) == 1) {
-	           sum += tree[l];
-	           l++;
-	        }
-	        if ((r % 2) == 0) {
-	           sum += tree[r];
-	           r--;
-	        }
-	        l /= 2;
-	        r /= 2;
-	    }
-	    return sum;
+		// get leaf with value 'l'
+		l += n;
+		// get leaf with value 'r'
+		r += n;
+		int sum = 0;
+		while (l <= r) {
+			if ((l % 2) == 1) {
+				sum += tree[l];
+				l++;
+			}
+			if ((r % 2) == 0) {
+				sum += tree[r];
+				r--;
+			}
+			l /= 2;
+			r /= 2;
+		}
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		int[] nums = { 2, 4, 5, 7, 8, 9};
+		NumArray n = new NumArray(nums);
+		System.out.println(sumRange(1, 4));
 	}
 }
