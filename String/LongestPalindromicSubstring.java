2e87ea3a3b374010c1ee43475969ab53828410ca
Han <hanyu2@asu.edu>
2016-08-03 12:18:06 -0700

    5

--- String/LongestPalindromicSubstring.java
+++ String/LongestPalindromicSubstring.java
@@ -1,29 +1,52 @@
 package String;
 
 public class LongestPalindromicSubstring {
-	static private int low, max;
+	 public String longestPalindrome(String s) {
+	        String res =  "";
+	        for(int i = 0; i < s.length(); i++){
+	            String s1 = check(s, i - 1, i);
+	            String s2 = check(s, i, i);
+	            if(s1.length() > res.length() || s2.length() > res.length()){
+	                res = s1.length() > s2.length()? s1: s2;
+	            }
+	        }
+	        return res;
+	    }
+	    
+	    public String check(String s, int left, int right){
+	        while(left >=0 && right < s.length()){
+	            if(s.charAt(left) == s.charAt(right)){
+	                left--;
+	                right++;
+	            }else{
+	                return s.substring(left + 1, right);
+	            }
+	        }
+	        return s.substring(left + 1, right);
+	    }
+	    //much faster
+	    public String longestPalindrome2(String s) {
+	        int start = 0, end = 0;
+	        for (int i = 0; i < s.length(); i++) {
+	            int len1 = expandAroundCenter(s, i, i);
+	            int len2 = expandAroundCenter(s, i, i + 1);
+	            int len = Math.max(len1, len2);
+	            if (len > end - start) {
+	                start = i - (len - 1) / 2;
+	                end = i + len / 2;
+	            }
+	        }
+	        return s.substring(start, end + 1);
+	    }
 
-	public static String longestPalindrome(String s) {
-		if (s.length() < 2) {
-			return s;
-		}
-		for (int i = 0; i < s.length() - 1; i++) {
-			extend(s, i, i);
-			extend(s, i, i + 1);
-		}
-		return s.substring(low, low + max);
-	}
-
-	private static void extend(String s, int start, int end) {
-		while (start >= 0 && end < s.length() && s.charAt(start) == s.charAt(end)) {
-			start--;
-			end++;
-		}
-		if (end - start - 1 > max) {
-			max = end - start - 1;
-			low = start + 1;
-		}
-	}
+	    private int expandAroundCenter(String s, int left, int right) {
+	        int L = left, R = right;
+	        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+	            L--;
+	            R++;
+	        }
+	        return R - L - 1;
+	    }
 
 	// DP
 	/*
@@ -32,7 +55,7 @@ public class LongestPalindromicSubstring {
 	 * palindromic substring. When we found a palindrome, check if it's the
 	 * longest one. Time complexity O(n^2).
 	 */
-	public static String longestPalindrome2(String s) {
+	public static String longestPalindrome3(String s) {
 		int n = s.length();
 		String res = null;
 
