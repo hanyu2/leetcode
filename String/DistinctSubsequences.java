7c956171a3761d46aed11e67714b14da539eb666
Han <hanyu2@asu.edu>
2016-08-06 11:24:47 -0700

    115

--- String/DistinctSubsequences.java
+++ String/DistinctSubsequences.java
@@ -2,6 +2,7 @@ package String;
 
 public class DistinctSubsequences {
 	//https://discuss.leetcode.com/topic/9488/easy-to-understand-dp-in-java/2
+	//http://www.cnblogs.com/higerzhang/p/4133793.html
 	public int numDistinct(String S, String T) {
 		// array creation
 		int[][] mem = new int[T.length() + 1][S.length() + 1];
@@ -26,9 +27,27 @@ public class DistinctSubsequences {
 		return mem[T.length()][S.length()];
 	}
 
+	//O(T) space O(TS) Time
+	public static int numDistinct2(String S, String T) {
+	    if (T.length() == 0)
+	        return 0;
+	        
+	    int[] counts = new int[T.length()];
+	    for (int indexS = 0; indexS < S.length(); indexS++) {
+	        char charS = S.charAt(indexS);
+	        for (int indexT = T.length() - 1; indexT >= 0; indexT--) {
+	            if (T.charAt(indexT) == charS) {
+	                if (indexT == 0)
+	                    counts[0]++;
+	                else
+	                    counts[indexT] += counts[indexT-1];
+	            }
+	        }
+	    }
+	    return counts[counts.length-1];
 	}
 
 	public static void main(String[] args) {
-		System.out.println(numDistinct("ccc", "c"));
+		System.out.println(numDistinct2("rabbbit", "rabbit"));
 	}
 }
