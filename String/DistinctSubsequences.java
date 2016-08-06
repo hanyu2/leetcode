4dfe16d1622e8be9aa3805a5c8218461dd3570ea
Han <hanyu2@asu.edu>
2016-08-06 10:47:03 -0700

    115

---
+++ String/DistinctSubsequences.java
@@ -0,0 +1,34 @@
+package String;
+
+public class DistinctSubsequences {
+	//https://discuss.leetcode.com/topic/9488/easy-to-understand-dp-in-java/2
+	public int numDistinct(String S, String T) {
+		// array creation
+		int[][] mem = new int[T.length() + 1][S.length() + 1];
+
+		// filling the first row: with 1s
+		for (int j = 0; j <= S.length(); j++) {
+			mem[0][j] = 1;
+		}
+
+		// the first column is 0 by default in every other rows but the first,
+		// which we need.
+
+		for (int i = 0; i < T.length(); i++) {
+			for (int j = 0; j < S.length(); j++) {
+				if (T.charAt(i) == S.charAt(j)) {
+					mem[i + 1][j + 1] = mem[i][j] + mem[i + 1][j];
+				} else {
+					mem[i + 1][j + 1] = mem[i + 1][j];
+				}
+			}
+		}
+		return mem[T.length()][S.length()];
+	}
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println(numDistinct("ccc", "c"));
+	}
+}
