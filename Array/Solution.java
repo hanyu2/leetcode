53dc570966bc571d721189290851bd53db0845ba
Han <hanyu2@asu.edu>
2016-08-11 09:51:58 -0700

    8

--- Array/Solution.java
+++ Array/Solution.java
@@ -5,38 +5,25 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
-	public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
-		int[] array = new int[nums1.length + nums2.length];
-		if (array.length == 0) {
-			return 0;
+	public static int reverse(int x) {
+		int sign = 1;
+		if (x < 0) {
+			sign = -1;
+			x = -x;
 		}
-		int i = 0, j = 0;
-		int t = 0;
-		while (i < nums1.length && j < nums2.length) {
-			if (nums1[i] < nums2[j]) {
-				array[t++] = nums1[i++];
-			} else {
-				array[t++] = nums2[j++];
+		int result = 0;
+		while (x != 0) {
+			if (result >= Integer.MAX_VALUE / 10) {
+				return 0;
 			}
+			result = result * 10 + x % 10;
+			x /= 10;
 		}
-		if (i < nums1.length) {
-			array[t++] = nums1[i++];
-		}
-		if (j < nums2.length) {
-			array[t] = nums2[j];
-			t++;
-			j++;
-		}
-		if (array.length % 2 == 1) {
-			return (double) array[array.length / 2];
-		} else {
-			return (double) (array[array.length / 2] + array[array.length / 2 - 1]) / 2;
-		}
+		return result * sign;
 	}
 
 	public static void main(String[] args) {
-		int[] nums1 = {};
-		int[] nums2 = { 2, 3 };
-		System.out.println(findMedianSortedArrays(nums1, nums2));
+
+		System.out.println(reverse(-2147483648));
 	}
 }
--- String/StringToInt.java
+++ String/StringToInt.java
@@ -36,6 +36,39 @@ public class StringToInt {
 		}
 		return (int) num * sign; ///////////notice cannot (int) sign *  num
 	}
+	//"  ++123" -> 0
+	public int myAtoi2(String str) {
+        str = str.trim();
+        int sign = 1;
+        char[] ch = str.toCharArray();
+        int res = 0;
+        for(int i = 0; i < str.length(); i++){
+            char c = ch[i];
+            if(c == '-'){
+                if(i > 0 && !Character.isDigit(ch[i -1])){
+                    return 0;
+                }
+                sign = -1;
+            }else if(c == '+'){
+                if(i > 0 && !Character.isDigit(ch[i -1])){
+                    return 0;
+                }
+                sign = 1;
+            }else{
+                if(c >= '0' && c <= '9'){
+                    if(sign == 1 && (res * 10 + Character.getNumericValue(c)) / 10 != res){
+                        return Integer.MAX_VALUE;
+                    }else if(sign == -1 &&(res * 10 + Character.getNumericValue(c)) / 10 != res){
+                        return Integer.MIN_VALUE;
+                    }
+                    res = res * 10 + Character.getNumericValue(c);
+                }else{
+                    return res * sign;
+                }
+            }
+        }
+        return res * sign;
+    }
 	
 	public static void main(String[] args) {
 		System.out.println(myAtoi("123"));
