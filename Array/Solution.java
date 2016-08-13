f324cf87c7aebea101756280e2f629acaec7fca9
Han <hanyu2@asu.edu>
2016-08-13 10:20:22 -0700

    29

--- Array/Solution.java
+++ Array/Solution.java
@@ -5,26 +5,39 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
-	public static int reverse(int x) {
-		int sign = 1;
-		int t = 0;
-		if (x < 0) {
-			sign = -1;
-			x = -x;
-		}
-		int result = 0;
-		while (x != 0) {
-			if (result >= Integer.MAX_VALUE / 10) {
-				return 0;
-			}
-			result = result * 10 + x % 10;
-			x /= 10;
-		}
-		return result * sign;
-	}
+	public static int divide(int dividend, int divisor) {
+        int sign = 1;
+        if((dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0)){
+            sign = -1;
+        }
+        long ldividend = Math.abs((long) dividend);
+	    long ldivisor = Math.abs((long) divisor);
+        long times = 0;
+        long num = 1;
+        while(ldividend - ldivisor * 2 >= 0){
+            ldivisor *= 2;
+            num *= 2;
+        }
+        while(ldividend > 0){
+            while(ldividend - ldivisor < 0){
+                ldivisor /= 2;
+                num /= 2;
+            }
+            ldividend -= ldivisor;
+            times += num;
+        }
+        if(times > Integer.MAX_VALUE){
+        	if(sign == 1){
+        		return Integer.MAX_VALUE;
+        	}else{
+        		return Integer.MIN_VALUE;
+        	}
+        }
+        return (int)times * sign;
+    }
 
 	public static void main(String[] args) {
 
-		System.out.println(reverse(-423458));
+		System.out.println(divide(-2147483648, -1));
 	}
 }
--- Math/DivideTwoIntegers.java
+++ Math/DivideTwoIntegers.java
@@ -42,6 +42,37 @@ public class DivideTwoIntegers {
 	    return multiple + ldivide(ldividend - sum, ldivisor);
 	}
 	
+	public static int divide2(int dividend, int divisor) {
+        int sign = 1;
+        if((dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0)){
+            sign = -1;
+        }
+        long ldividend = Math.abs((long) dividend);
+	    long ldivisor = Math.abs((long) divisor);
+        long times = 0;
+        long num = 1;
+        while(ldividend - ldivisor * 2 >= 0){
+            ldivisor *= 2;
+            num *= 2;
+        }
+        while(ldividend > 0){
+            while(ldividend - ldivisor < 0){
+                ldivisor /= 2;
+                num /= 2;
+            }
+            ldividend -= ldivisor;
+            times += num;
+        }
+        if(times > Integer.MAX_VALUE){
+        	if(sign == 1){
+        		return Integer.MAX_VALUE;
+        	}else{
+        		return Integer.MIN_VALUE;
+        	}
+        }
+        return (int)times * sign;
+    }
+	
 	public static void main(String[] args) {
 		System.out.println(divide(65, -13));
 	}
