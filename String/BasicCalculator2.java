7b888c3b7a3967e5e07a11bc3097ec2714f26b78
hanyu <hanyu2@asu.edu>
2016-09-16 10:25:50 -0700

    227

--- String/BasicCalculator2.java
+++ String/BasicCalculator2.java
@@ -1,5 +1,7 @@
 package String;
 
+import java.util.Stack;
+
 public class BasicCalculator2 {
 	public static int calculate(String s) {
 		s = s.replaceAll("\\s+", "");
@@ -41,6 +43,40 @@ public class BasicCalculator2 {
         }
         return (int)result;
     }
+	public int calculate2(String s) {
+	    int len;
+	    if(s==null || (len = s.length())==0) return 0;
+	    Stack<Integer> stack = new Stack<Integer>();
+	    int num = 0;
+	    char sign = '+';
+	    for(int i=0;i<len;i++){
+	        if(Character.isDigit(s.charAt(i))){
+	            num = num*10+s.charAt(i)-'0';
+	        }
+	        if((!Character.isDigit(s.charAt(i)) &&' '!=s.charAt(i)) || i==len-1){
+	            if(sign=='-'){
+	                stack.push(-num);
+	            }
+	            if(sign=='+'){
+	                stack.push(num);
+	            }
+	            if(sign=='*'){
+	                stack.push(stack.pop()*num);
+	            }
+	            if(sign=='/'){
+	                stack.push(stack.pop()/num);
+	            }
+	            sign = s.charAt(i);
+	            num = 0;
+	        }
+	    }
+
+	    int re = 0;
+	    for(int i:stack){
+	        re += i;
+	    }
+	    return re;
+	}
 	public static void main(String[] args) {
 		String s = "3+2*2-1";
 		String s2 = "3/2";
