de2ea2e59c6f839cac34720d0fb4553cd55d936a
hanyu <hanyu2@asu.edu>
2016-10-11 21:25:43 -0700

    128

--- DP/Triangle.java
+++ DP/Triangle.java
@@ -1,5 +1,30 @@
 package DP;
 
-public class Triangle {
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
 
+public class Triangle {
+	public int minimumTotal(List<List<Integer>> triangle) {
+        Deque<Integer> queue = new LinkedList<Integer>();
+        int count=triangle.size();
+        queue.add(triangle.get(0).get(0));
+        for (int i=1;i<count;i++){
+            List<Integer> list= triangle.get(i);
+            for (int j=0;j<=i;j++){
+            	int min=0;
+                if (j==0)
+                	 min=list.get(0)+queue.peekFirst();               	
+                else if (j==i)
+                	 min =list.get(j)+queue.pollFirst();              	
+                else
+                	min = Math.min(queue.pollFirst(),queue.peekFirst())+list.get(j);              	               
+                queue.addLast(min);               
+            }
+        }
+        int result=Integer.MAX_VALUE;
+        for (int i=0;i<count;i++)
+        	result=Math.min(result, queue.pollFirst());
+        return result;
+    }
 }
---
+++ UnionFind/LongestConsecutiveSequence.java
@@ -0,0 +1,97 @@
+package UnionFind;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestConsecutiveSequence {
+	public static int longestConsecutive(int[] num) {
+	    int res = 0;
+	    HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+	    for (int n : num) {
+	        if (!map.containsKey(n)) {
+	            int left = (map.containsKey(n - 1)) ? map.get(n - 1) : 0;
+	            int right = (map.containsKey(n + 1)) ? map.get(n + 1) : 0;
+	            // sum: length of the sequence n is in
+	            int sum = left + right + 1;
+	            map.put(n, sum);
+	            
+	            // keep track of the max length 
+	            res = Math.max(res, sum);
+	            
+	            // extend the length to the boundary(s)
+	            // of the sequence
+	            // will do nothing if n has no neighbors
+	            map.put(n - left, sum);
+	            map.put(n + right, sum);
+	        }
+	        else {
+	            // duplicates
+	            continue;
+	        }
+	    }
+	    return res;
+	}
+	//Union Find
+	 public int longestConsecutive2(int[] nums) {
+         UF uf = new UF(nums.length);
+         Map<Integer,Integer> map = new HashMap<Integer,Integer>(); // <value,index>
+         for(int i=0; i<nums.length; i++){
+             if(map.containsKey(nums[i])){
+                 continue;
+             }
+             map.put(nums[i],i);
+             if(map.containsKey(nums[i]+1)){
+                 uf.union(i,map.get(nums[i]+1));
+             }
+             if(map.containsKey(nums[i]-1)){
+                 uf.union(i,map.get(nums[i]-1));
+             }
+         }
+         return uf.maxUnion();
+     }
+ 
+	
+	public static void main(String[] args){
+		int num[] = {100, 4, 200, 1, 3, 2};
+		longestConsecutive(num);
+	}
+}
+
+class UF{
+    private int[] list;
+    public UF(int n){
+        list = new int[n];
+        for(int i=0; i<n; i++){
+            list[i] = i;
+        }
+    }
+    
+    private int root(int i){
+        while(i!=list[i]){
+            list[i] = list[list[i]];
+            i = list[i];
+        }
+        return i;
+    }
+    
+    public boolean connected(int i, int j){
+        return root(i) == root(j);
+    }
+    
+    public void union(int p, int q){
+      int i = root(p);
+      int j = root(q);
+      list[i] = j;
+    }
+    
+    // returns the maxium size of union
+    public int maxUnion(){ // O(n)
+        int[] count = new int[list.length];
+        int max = 0;
+        for(int i=0; i<list.length; i++){
+            count[root(i)] ++;
+            max = Math.max(max, count[root(i)]);
+        }
+        return max;
+    }
+}
