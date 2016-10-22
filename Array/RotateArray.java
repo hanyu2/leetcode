ec61a45f055eb14ed80e44cf99a502db2012f661
hanyu <hanyu2@asu.edu>
2016-10-22 12:57:41 -0700

    ADD DB

--- Array/RotateArray.java
+++ Array/RotateArray.java
@@ -2,26 +2,20 @@ package Array;
 
 public class RotateArray {
 	public static void rotate(int[] nums, int k) {
-		if (nums == null || nums.length == 0) {
-			return;
-		}
-		int len = nums.length;
-		k = k % len;
-
-		reverse(nums, 0, len - k - 1);
-		reverse(nums, len - k, len - 1);
-		reverse(nums, 0, len - 1);
-	}
-
-	static void reverse(int[] nums, int start, int end) {
-		while (start < end) {
-			int temp = nums[start];
-			nums[start] = nums[end];
-			nums[end] = temp;
-			start++;
-			end--;
-		}
-	}
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+    public static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
 
 	public static void main(String[] args) {
 		int array [] = {1, 2};
