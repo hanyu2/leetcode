9896efacef720bf3e62f119c44e917765d1b7307
hanyu <hanyu2@asu.edu>
2016-10-30 11:16:51 -0700

    161

--- FB/OneEditDistance.java
+++ FB/OneEditDistance.java
@@ -1,26 +1,24 @@
 package FB;
 
 public class OneEditDistance {
-	public boolean edit(String s1, String s2) {
-	    if(s2.length() < s1.length()) {
-	        return edit(s2, s1);
-	    }        
-	    if(s1.length() + 1 < s2.length()) {
-	        return false;
-	    }
-
-	    int i = 0;
-	    while(i < s1.length()) {
-	        if(s1.charAt(i) != s2.charAt(i)) {
-	            if(s1.length() == s2.length()) {
-	                return s1.substring(i+1).equals(s2.substring(i+1));
-	            }else {
-	                return s1.substring(i).equals(s2.substring(i+1));
-	            }
-	        }
-	        i++;
-	    }
-	    //this is for the last char
-	    return s1.length() != s2.length();
-	}
+	public boolean isOneEditDistance(String s, String t) {
+        if(s.length() > t.length()){
+            return isOneEditDistance(t, s);
+        }
+        if(Math.abs(t.length() - s.length()) > 1){
+            return false;
+        }
+        int i = 0;
+        while(i < s.length()){
+            if(s.charAt(i) != t.charAt(i)){
+                if(s.length() == t.length()){
+                    return s.substring(i + 1).equals(t.substring(i + 1));
+                }else{
+                    return s.substring(i).equals(t.substring(i + 1));
+                }
+            }
+            i++;
+        }
+        return s.length() != t.length();
+    }
 }
