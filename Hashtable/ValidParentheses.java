26192ef07b321c685ce8f6fc4783200efb3333d0
hanyu <hanyu2@asu.edu>
2016-11-03 11:31:56 -0700

    20

--- Hashtable/ValidParentheses.java
+++ Hashtable/ValidParentheses.java
@@ -26,8 +26,17 @@ public class ValidParentheses {
 		return (c1 == '{' && c2 == '}') || (c1 == '[' && c2 == ']') || (c1 == '(' && c2 == ')');
 	}
 
+	public static boolean isValid2(String s) {
+		while (s.contains("()") || s.contains("[]") || s.contains("{}")) {
+			s = s.replace("()", "");
+			s = s.replace("[]", "");
+			s = s.replace("{}", "");
+		}
+		return s.length() == 0;
+	}
+
 	public static void main(String[] args) {
-		String s = "([)]";
-		System.out.println(isValid(s));
+		String s = "()";
+		System.out.println(isValid2(s));
 	}
 }
