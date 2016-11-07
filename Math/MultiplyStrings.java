0e27fb6b02e9409c635d42fe576f1ddaae0ccdd6
hanyu <hanyu2@asu.edu>
2016-11-07 16:09:57 -0700

    43

--- Math/MultiplyStrings.java
+++ Math/MultiplyStrings.java
@@ -4,25 +4,30 @@ public class MultiplyStrings {
 	// https://leetcode.com/discuss/71593/easiest-java-solution-with-graph-explanation
 	public static String multiply(String num1, String num2) {
 		int m = num1.length(), n = num2.length();
-		int[] pos = new int[m + n];
+		int[] res = new int[m + n];
 
 		for (int i = m - 1; i >= 0; i--) {
 			for (int j = n - 1; j >= 0; j--) {
 				int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
 				int p1 = i + j, p2 = i + j + 1;
-				int sum = mul + pos[p2];
+				int sum = mul + res[p2];
 
-				pos[p1] += sum / 10;// pay attention!!!
-				pos[p2] = (sum) % 10;
+				res[p1] += sum / 10;// pay attention!!!
+				res[p2] = (sum) % 10;
 			}
 		}
 
 		StringBuilder sb = new StringBuilder();
-		for (int p : pos) {
-			if (!(sb.length() == 0 && p == 0))
-				sb.append(p);
-		}
-		return sb.length() == 0 ? "0" : sb.toString();
+        boolean zero = true;
+        for(int i = 0; i < m + n; i++){
+            if(res[i] != 0){
+                zero = false;
+                sb.append(res[i]);
+            }else if(!zero){
+                sb.append(res[i]);
+            }
+        }
+        return sb.length() == 0 ?  "0" : sb.toString();
 	}
 
 	public static void main(String[] args) {
