26f736fdddee385f082d2f5f46af55373b332ca5
hanyu <hanyu2@asu.edu>
2016-11-08 11:52:05 -0700

    69

--- BinarySearch/Sqrt.java
+++ BinarySearch/Sqrt.java
@@ -4,20 +4,24 @@ public class Sqrt {
 	
 	//If (m * m) overflows the 32 bit int limit, you might end up with an infinite loop.
 	public static int mySqrt(int x) {
-		int left = 1, right = x;
-	    while (left <= right) {
-	        int mid = left + (right - left) / 2;
-	        if (mid == x / mid) {
-	            return mid;
-	        } else if (mid < x / mid) {
-	            left = mid + 1;
-	        } else {
-	            right = mid - 1;
-	        }
-	    }
-	    return right;
+		if(x <= 1){
+			return x;
+		}
+		int left = 1;
+		int right = x / 2;
+		int res = 0;
+		while(left <= right){
+			int mid = (left + right) / 2;
+			if(mid <= x / mid){
+				res = mid;
+				left = mid + 1;
+			}else{
+				right = mid - 1;
+			}
+		}
+		return res;
 	}
 	public static void main(String[] args) {
-		System.out.println(mySqrt(10));
+		System.out.println(mySqrt(6));
 	}
 }
