53b6ffdf047562746c6cc3f1ceb9a5f0918d9718
hanyu <hanyu2@asu.edu>
2016-11-10 11:53:26 -0700

    127

--- BFS/Solution.java
+++ BFS/Solution.java
@@ -1,49 +1,34 @@
 package BFS;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import com.sun.xml.internal.fastinfoset.algorithm.FloatEncodingAlgorithm;
-
-import sun.print.resources.serviceui;
+import Tree.TreeNode;
 
 public class Solution {
-	public static void setZeroes(int[][] matrix) {
-		if (matrix.length == 0) {
-			return;
-		}
-		boolean set = false;
-		int m = matrix.length;
-		int n = matrix[0].length;
-		for (int i = 0; i < m; i++) {
-			for (int j = 0; j < n; j++) {
-				if (matrix[i][0] == 0) {
-					set = true;
-				}
-				if (matrix[i][j] == 0) {
-					matrix[i][0] = 0;
-					matrix[0][j] = 0;
-				}
-			}
-		}
-		for (int i = 0; i < m; i++) {
-			for (int j = 1; j < n; j++) {
-				if (matrix[i][0] == 0 || matrix[0][j] == 0) {
-					matrix[i][j] = 0;
-				}
-			}
-			if (set) {
-				matrix[i][0] = 0;
-			}
-		}
-	}
-
+	static int max = Integer.MIN_VALUE;
+    public static int maxPathSum(TreeNode root) {
+        maxSum(root);
+        return max;
+    }
+    
+    public static int maxSum(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        int left = maxSum(root.left);
+        int right = maxSum(root.right);
+        if(left >= 0 && right >= 0){
+            max = Math.max(max, root.val + left + right);
+        }
+        if(left < 0 && right < 0){
+            return root.val;
+        }else{
+            return root.val + Math.max(left, right);
+        }
+    }
 
 	public static void main(String[] args) {
-		int[][] matrix = {{0,0,0,5},{4,3,1,4},{0,1,1,4},{1,2,1,3},{0,0,1,1}};
-		System.out.println(Float.parseFloat("1000.000000"));
-		int i = 0;
-		
+		TreeNode n1 = new TreeNode(2);
+		TreeNode n2 = new TreeNode(-1);
+		n1.right = n2;
+		maxPathSum(n1);
 	}
 }
--- BFS/WordLadder.java
+++ BFS/WordLadder.java
@@ -71,30 +71,25 @@ public class WordLadder {
 			Set<String> temp = new HashSet<String>();
 			for (String word : beginSet) {
 				char[] chs = word.toCharArray();
-
 				for (int i = 0; i < chs.length; i++) {
+					char old = chs[i];
 					for (char c = 'a'; c <= 'z'; c++) {
-						char old = chs[i];
 						chs[i] = c;
 						String target = String.valueOf(chs);
-
 						if (endSet.contains(target)) {
 							return len + 1;
 						}
-
 						if (!visited.contains(target) && wordList.contains(target)) {
 							temp.add(target);
 							visited.add(target);
 						}
-						chs[i] = old;
 					}
+					chs[i] = old;
 				}
 			}
-
 			beginSet = temp;
 			len++;
 		}
-
 		return 0;
 	}
 }
