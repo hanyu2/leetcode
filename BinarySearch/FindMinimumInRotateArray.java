e85b27e8bffb8fc280e4107a99b732b0a3572f18
hanyu <hanyu2@asu.edu>
2016-11-11 11:00:55 -0700

    153

--- BinarySearch/FindMinimumInRotateArray.java
+++ BinarySearch/FindMinimumInRotateArray.java
@@ -1,7 +1,19 @@
 package BinarySearch;
 
 public class FindMinimumInRotateArray {
-	
+
+	public int findMin2(int[] nums) {
+		int start = 0, end = nums.length - 1, mid;
+		while (start < end) {
+			mid = (start + end) / 2;
+			if (nums[mid] > nums[end])
+				start = mid + 1;
+			else
+				end = mid;
+		}
+		return nums[start];
+	}
+
 	public static int findMin(int[] nums) {
 		if (nums == null || nums.length == 0) {
 			return 0;
@@ -23,19 +35,7 @@ public class FindMinimumInRotateArray {
 		}
 		return nums[start];
 	}
-
-	public int findMin2(int[] nums) {
-		int start = 0, end = nums.length - 1, mid;
-		while (start < end) {
-			mid = (start + end) / 2;
-			if (nums[mid] > nums[end])
-				start = mid + 1;
-			else
-				end = mid;
-		}
-		return nums[start];
-	}
-
+	
 	public static void main(String[] args) {
 		int[] nums = {3, 1, 2};
 		System.out.println(findMin(nums));
