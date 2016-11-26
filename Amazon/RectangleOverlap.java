bd7f8893b46f7aae583ad84669e3ccb8f74c5d7d
hanyu <hanyu2@asu.edu>
2016-11-26 15:02:33 -0700

    RectangleOverlap

--- Amazon/RectangleOverlap.java
+++ Amazon/RectangleOverlap.java
@@ -3,23 +3,34 @@ package Amazon;
 public class RectangleOverlap {
 	// rectangle A, B
 	// time O(1), space O(1)
-	public static boolean check(Node topLeftA, Node topLeftB, Node bottomRightA, Node bottomRightB) {
-		if (bottomRightA.y >= topLeftB.y || bottomRightB.y >= topLeftA.y) {
+	public static boolean overlap(Rect rect1, Rect rect2) {
+		int rect1LeftTopX = Math.min(rect1.leftTop.x, rect1.rightBottom.x);
+		int rect1LeftTopY = Math.max(rect1.leftTop.x, rect1.rightBottom.x);
+		int rect1rightBottomX = Math.max(rect1.leftTop.x, rect1.rightBottom.x);
+		int rect1rightBottomY = Math.min(rect1.leftTop.x, rect1.rightBottom.x);
+
+		int rect2LeftTopX = Math.min(rect2.leftTop.x, rect2.rightBottom.x);
+		int rect2LeftTopY = Math.max(rect2.leftTop.x, rect2.rightBottom.x);
+		int rect2rightBottomX = Math.max(rect2.leftTop.x, rect2.rightBottom.x);
+		int rect2rightBottomY = Math.min(rect2.leftTop.x, rect2.rightBottom.x);
+
+		if (rect1LeftTopX > rect2rightBottomX || rect2LeftTopX > rect1rightBottomX) {
 			return false;
 		}
-		if (topLeftA.x >= bottomRightB.x || topLeftB.x >= bottomRightA.x) {
+		if (rect1rightBottomY > rect2LeftTopY || rect2rightBottomY > rect1LeftTopY) {
 			return false;
 		}
 		return true;
+
 	}
 
-	public static class Node {
-		double x;
-		double y;
+	class Node {
+		int x;
+		int y;
+	}
 
-		public Node(double x, double y) {
-			this.x = x;
-			this.y = y;
-		}
+	class Rect {
+		Node leftTop;
+		Node rightBottom;
 	}
 }
