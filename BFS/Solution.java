3ce689a2f3ed3c6f69374decf6b7004da377563f
hanyu <hanyu2@asu.edu>
2016-12-31 17:55:20 -0700

    139

--- BFS/Solution.java
+++ BFS/Solution.java
@@ -1,28 +1,21 @@
 package BFS;
 
 public class Solution {
-	public static int mySqrt(int x) {
-        int left = 0;
-        int right = Integer.MAX_VALUE;
-        while(left <= right){
-            int mid = left + (right - left) / 2;
-            if(mid > x / mid){
-                right = mid - 1;
-            }else{
-                if(mid == x / mid){
-                    return mid;
-                }else{
-                    if(mid >= x / (mid + 1)){
-                        return mid;
-                    }else{
-                        left = mid + 1;
-                    }
+	public static int combinationSum4(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+        for(int  i = 1; i <= target; i++){
+            for(int j = 0; j < nums.length; j++){
+                if(target - nums[j] == 0){
+                    dp[i] += 1;
+                }else if(target - nums[j] > 0){
+                    dp[i] += dp[target - nums[j]];
                 }
             }
         }
-        return 0;
+        return dp[target];
     }
 	public static void main(String[] args) {
-		System.out.println(mySqrt(4));
+		int[] nums = {1, 2, 3};
+		System.out.println(combinationSum4(nums, 4));
 	}
 }
