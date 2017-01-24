2948401a96556ef63e5121ae3bbd3e944459f833
hanyu <hanyu2@asu.edu>
2017-01-24 16:37:19 -0700

    35

--- BinarySearch/SearchInsertPoint.java
+++ BinarySearch/SearchInsertPoint.java
@@ -1,6 +1,7 @@
 package BinarySearch;
 
 public class SearchInsertPoint {
+	//first position >= target
 	public static int searchInsert(int[] nums, int target) {
 		int low = 0, high = nums.length - 1;
 		while (low <= high) {
@@ -13,6 +14,28 @@ public class SearchInsertPoint {
 				low = mid + 1;
 		}
 		return low;
+	}
+	
+	public static int searchInsert2(int[] nums, int target) {
+		int start = 0;
+		int end = nums.length - 1;
+		while(start + 1 < end){
+			int mid = start + (end - start) / 2;
+			if(nums[mid] == target){
+				return mid;
+			}else if(nums[mid] < target){
+				start = mid;
+			}else {
+				end = mid;
+			}
+		}
+		if(nums[start] >= target){
+			return start;
+		}else if(nums[end] >= target){
+			return end;
+		}else {
+			return nums.length;
+		}
 	}
 
 	public static void main(String[] args) {
