9aa29c63617ae0b186c8b619b5e942549913a2b4
hanyu <hanyu2@asu.edu>
2017-01-25 14:43:40 -0700

    236

--- Tree/LowestCommonAncestor.java
+++ Tree/LowestCommonAncestor.java
@@ -24,6 +24,25 @@ public class LowestCommonAncestor {
 		}
 		return cover(root.left, p) || cover(root.right, p);
 	}
+	
+	//nine chapter
+	public TreeNode lowestCommonAncestor22(TreeNode root, TreeNode p, TreeNode q) {
+	       if(root == null || root == p || root == q){
+	           return root;
+	       } 
+	       TreeNode left = lowestCommonAncestor(root.left, p, q);
+	       TreeNode right = lowestCommonAncestor(root.right, p, q);
+	       if(left != null && right != null){
+	           return root;
+	       }
+	       if(left != null){
+	           return left;
+	       }
+	       if(right != null){
+	           return right;
+	       }
+	       return null;
+	    }
 
 	public static TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
 		if (root == null) {
