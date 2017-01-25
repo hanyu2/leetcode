7cc583962ed1c8e2e1c8314862bf4bd189225b15
hanyu <hanyu2@asu.edu>
2017-01-24 21:05:34 -0700

    4

--- BinarySearch/MedianOfTwoSortedArrays.java
+++ BinarySearch/MedianOfTwoSortedArrays.java
@@ -43,6 +43,35 @@ public class MedianOfTwoSortedArrays {
 		return (double)(maxLeft + minRight) / 2;
 	}
 	
+	//O(log k) nine chapter
+	public double findMedianSortedArrays2(int[] nums1, int[] nums2) {
+        int length = nums1.length + nums2.length;
+        if(length % 2 == 0){
+            return (findKthLargest(nums1, 0, nums2, 0, length / 2) + findKthLargest(nums1, 0, nums2, 0, length / 2 + 1)) / 2.0;
+        }else{
+            return findKthLargest(nums1, 0, nums2, 0, length / 2 + 1);
+        }
+    }
+    
+    public int findKthLargest(int[] nums1, int start1, int[] nums2, int start2, int k){
+        if(start1 >= nums1.length){
+            return nums2[start2 + k - 1];
+        }
+        if(start2 >= nums2.length){
+            return nums1[start1 + k - 1];
+        }
+        if(k == 1){
+            return Math.min(nums1[start1], nums2[start2]);
+        }
+        int keyOfNums1 = start1 + k / 2 - 1 < nums1.length ? nums1[start1 + k / 2 - 1] : Integer.MAX_VALUE;
+        int keyOfNums2 = start2 + k / 2 - 1 < nums2.length ? nums2[start2 + k / 2 - 1] : Integer.MAX_VALUE;
+        if(keyOfNums1 < keyOfNums2){
+            return findKthLargest(nums1, start1 + k / 2, nums2, start2, k - k / 2);
+        }else{
+            return findKthLargest(nums1, start1, nums2, start2 + k / 2, k - k / 2);
+        }
+    }
+	
 	public static void main(String[] args) {
 		int []nums1 = {2};
 		int []nums2 = {};
